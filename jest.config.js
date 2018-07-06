30f0781f671d168c10a63db05fa053bc11aa7270
Jed Fox <git@twopointzero.us>
2018-07-06 19:49:53 -0400

    Install Jest and add a test

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const ENABLE_COVERAGE = !!process.env.CI;
+
+const semver = require("semver");
+const isOldNode = semver.parse(process.version).major <= 4;
+
+module.exports = {
+  setupFiles: ["<rootDir>/tests_config/run_spec.js"],
+  snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
+  testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
+  collectCoverage: ENABLE_COVERAGE,
+  collectCoverageFrom: ["src/**/*.js", "!<rootDir>/node_modules/"],
+  testEnvironment: "node",
+  transform: {},
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ]
+};
+
