ceb4a6eee4937d01e029a4da830c0631a19627af
mattia richetto <mattia.richetto@gmail.com>
2018-07-06 22:30:24 +0200

    moar prettier ?!?

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 // https://prettier.io/docs/en/plugins.html#parsers
 const parser = require('solidity-parser-antlr');
 
-const parse = text => parser.parse(text, { loc: true });
+const parse = text => parser.parse(text, { loc: true, range: true });
 
 module.exports = parse;
