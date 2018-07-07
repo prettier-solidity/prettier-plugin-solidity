f03ee6d24fe93da5c121bb46fa3533a01cd0a915
Jed Fox <git@twopointzero.us>
2018-07-07 10:44:50 -0400

    Remove unused variables and inline CI check

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
 'use strict';
 
-const ENABLE_COVERAGE = !!process.env.CI;
-
-const semver = require('semver');
-const isOldNode = semver.parse(process.version).major <= 4;
-
 module.exports = {
   setupFiles: ['<rootDir>/tests_config/run_spec.js'],
   snapshotSerializers: ['<rootDir>/tests_config/raw-serializer.js'],
   testRegex: 'jsfmt\\.spec\\.js$|__tests__/.*\\.js$',
-  collectCoverage: ENABLE_COVERAGE,
+  collectCoverage: !!process.env.CI,
   collectCoverageFrom: ['src/**/*.js', '!<rootDir>/node_modules/'],
   testEnvironment: 'node',
   transform: {},
