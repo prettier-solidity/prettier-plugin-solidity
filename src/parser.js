4dc56ddb8952647cdc03a86518ce666c907ab9ff
Franco Victorio <victorio.franco@gmail.com>
2018-09-27 08:20:13 -0300

    Remove unused properties from parsed comments

--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,15 @@ const parser = require('solidity-parser-antlr');
 const parse = text => {
   const parsed = parser.parse(text, { loc: true, range: true });
   parsed.comments = extract(text);
+
+  // `code` and `codeStart` are different after prettifying, so the tests fail when they have AST_COMPARE enabled.
+  // Since we don't use those properties, we just remove them.
+  parsed.comments.forEach(comment => {
+    delete comment.code;
+    delete comment.codeStart;
+    return comment;
+  });
+
   return parsed;
 };
 
