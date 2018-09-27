f73ba50d1104a4bf4ede98210e1e771372e9dd8f
mattia richetto <mattia.richetto@gmail.com>
2018-09-27 15:29:52 +0200

    update clean

--- src/parser.js
+++ src/parser.js
@@ -5,12 +5,6 @@ const parser = require('solidity-parser-antlr');
 const parse = text => {
   const parsed = parser.parse(text, { loc: true, range: true });
   parsed.comments = extract(text);
-  parsed.comments.forEach(comment => {
-    // info: remove unused properties
-    delete comment.code; // eslint-disable-line no-param-reassign
-    delete comment.codeStart; // eslint-disable-line no-param-reassign
-  });
-
   return parsed;
 };
 
