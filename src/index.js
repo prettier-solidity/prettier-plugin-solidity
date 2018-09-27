3a3cad81b01fa54556a71ca3f8243fde07defda0
mattia richetto <mattia.richetto@gmail.com>
2018-09-27 15:39:28 +0200

    preserve-comments (#40)
    * something working !?!
    
    * Use comment.raw property instead of value
    
    * Simplify locStart and locEnd
    
    * Simplify printComment
    
    * Update snapshots
    
    * Remove unused properties from parsed comments
    
    * Fix linter errors
    
    * moar eslint
    
    * add Franco Victorio as contributor
    
    * v1.0.0-alpha.4
    
    * update clean

--- src/index.js
+++ src/index.js
@@ -15,15 +15,38 @@ const languages = [
 const parsers = {
   'solidity-parse': {
     astFormat: 'solidity-ast',
+    locEnd: node => node.range[1],
+    locStart: node => node.range[0],
     parse
   }
 };
 
+function canAttachComment(node) {
+  return (
+    node.type && node.type !== 'BlockComment' && node.type !== 'LineComment'
+  );
+}
+
+function printComment(commentPath) {
+  const comment = commentPath.getValue();
+  switch (comment.type) {
+    case 'BlockComment': {
+      return `/*${comment.raw}*/`;
+    }
+    case 'LineComment':
+      return `//${comment.raw.trimRight()}`;
+    default:
+      throw new Error(`Not a comment: ${JSON.stringify(comment)}`);
+  }
+}
+
 // https://prettier.io/docs/en/plugins.html#printers
 const printers = {
   'solidity-ast': {
+    canAttachComment,
+    massageAstNode,
     print,
-    massageAstNode
+    printComment
   }
 };
 
--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,11 @@
+const extract = require('extract-comments');
 // https://prettier.io/docs/en/plugins.html#parsers
 const parser = require('solidity-parser-antlr');
 
-const parse = text => parser.parse(text, { loc: true, range: true });
+const parse = text => {
+  const parsed = parser.parse(text, { loc: true, range: true });
+  parsed.comments = extract(text);
+  return parsed;
+};
 
 module.exports = parse;
