4a29f1ac8b34435300aa9ed2f71027a45ff785e2
mattia richetto <mattia.richetto@gmail.com>
2018-10-16 21:07:35 -0300

    s/reduce/map

--- __tests__/nodes/index.test.js
+++ __tests__/nodes/index.test.js
@@ -1,10 +1,7 @@
 const nodes = require('../../src/nodes');
 
 test('nodes list to match snapshot', () => {
-  const list = Object.keys(nodes).reduce((accumulator, current) => {
-    accumulator.push(current);
-    return accumulator;
-  }, []);
+  const list = Object.keys(nodes).map(key => key);
 
   expect(list).toMatchSnapshot();
 });
