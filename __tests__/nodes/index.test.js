1d55aa8b0b4055dabf5ebf4416960add6898078e
mattia richetto <mattia.richetto@gmail.com>
2018-10-16 21:09:16 -0300

    map reduce no more

--- __tests__/nodes/index.test.js
+++ __tests__/nodes/index.test.js
@@ -1,7 +1,5 @@
 const nodes = require('../../src/nodes');
 
 test('nodes list to match snapshot', () => {
-  const list = Object.keys(nodes).map(key => key);
-
-  expect(list).toMatchSnapshot();
+  expect(Object.keys(nodes)).toMatchSnapshot();
 });
