1b2977084296ce2ffb239260d00ef8306ebf8f62
Mudit Gupta <guptamudit@ymail.com>
2019-01-11 12:11:35 +0530

    Fixed a tupple error

--- src/printer.js
+++ src/printer.js
@@ -31,6 +31,9 @@ function printPreservingEmptyLines(path, key, options, print) {
 function genericPrint(path, options, print) {
   const node = path.getValue();
   let doc;
+  if (node === null)
+    return ''
+    
   switch (node.type) {
     case 'SourceUnit':
       return concat([
