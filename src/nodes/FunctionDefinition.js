b3a8ea50d9cd19210e4deb623690e2439b171f0c
Klaus Hott Vidal <klahott@gmail.com>
2019-06-04 15:52:37 +1200

    refactor of Function definition and addressing returns alignment

--- src/nodes/FunctionDefinition.js
+++ src/nodes/FunctionDefinition.js
@@ -4,65 +4,73 @@ const {
   }
 } = require('prettier');
 
-const FunctionDefinition = {
-  print: ({ node, path, print }) => {
-    let parts = [];
-
-    if (node.isConstructor) {
-      if (node.name) {
-        parts.push(`function ${node.name}`);
-      } else {
-        parts.push('constructor');
-      }
-    } else if (node.name === '') {
-      parts.push('function');
-    } else {
-      parts = parts.concat(['function ', node.name]);
-    }
-
-    parts = parts.concat(['(', path.call(print, 'parameters'), ')']);
-
-    let modifiers = [];
-    if (node.visibility && node.visibility !== 'default') {
-      modifiers.push(node.visibility);
-    }
-    // @TODO: check stateMutability null vs default
-    if (node.stateMutability && node.stateMutability !== 'default') {
-      modifiers.push(node.stateMutability);
-    }
-    if (node.modifiers.length > 0) {
-      modifiers = modifiers.concat(path.map(print, 'modifiers'));
-    }
-    if (node.returnParameters) {
-      modifiers.push(
-        concat(['returns (', path.call(print, 'returnParameters'), ')'])
-      );
-    }
-
-    if (modifiers.length > 0) {
-      parts.push(
-        group(
-          concat(
-            [
-              indent(line),
-              join(indent(line), modifiers),
-              node.body ? line : null
-            ].filter(x => x)
-          )
-        )
-      );
-    } else if (node.body) {
-      parts.push(' ');
-    }
-
-    if (node.body) {
-      parts.push(path.call(print, 'body'));
-    } else {
-      parts.push(';');
-    }
-
-    return concat(parts);
+const functionName = node => {
+  if (node.isConstructor && !node.name) {
+    return 'constructor';
+  } else if (node.name) {
+    return `function ${node.name}`;
+  } else {
+    return 'function';
   }
+};
+
+const visibility = node => {
+  if (node.visibility && node.visibility !== 'default')
+    return concat([line, node.visibility]);
+};
+
+const stateMutability = node => {
+  if (node.stateMutability && node.stateMutability !== 'default')
+    return concat([line, node.stateMutability]);
+};
+
+const modifiers = (node, path, print) => {
+  if (node.modifiers.length > 0)
+    return concat([line, join(line, path.map(print, 'modifiers'))]);
+};
+
+const returnParameters = (node, path, print) => {
+  if (node.returnParameters)
+    return concat([
+      line,
+      'returns (',
+      path.call(print, 'returnParameters'),
+      ')'
+    ]);
+};
+
+const abstractFunctionSemicolon = node => {
+  if (!node.body) return ';';
+};
+
+const body = (node, path, print) => {
+  if (node.body) return concat([' ', path.call(print, 'body')]);
+};
+
+const FunctionDefinition = {
+  print: ({ node, path, print }) =>
+    concat(
+      [
+        functionName(node),
+        '(',
+        path.call(print, 'parameters'),
+        ')',
+        indent(
+          group(
+            concat(
+              [
+                visibility(node),
+                stateMutability(node),
+                modifiers(node, path, print),
+                returnParameters(node, path, print),
+                abstractFunctionSemicolon(node)
+              ].filter(element => element)
+            )
+          )
+        ),
+        body(node, path, print)
+      ].filter(element => element)
+    )
 };
 
 module.exports = FunctionDefinition;
