79bae0c40403a1032662814acade20295dda7fdb
fvictorio <victorio.franco@gmail.com>
2019-06-07 10:40:40 -0300

    Remove TODO comment

--- src/nodes/StringLiteral.js
+++ src/nodes/StringLiteral.js
@@ -1,6 +1,5 @@
 const { printString } = require('../prettier-comments/common/util');
 
-// @TODO: handle scaping, single/double quotes, etc.
 const StringLiteral = {
   print: ({ node, options }) => printString(node.value, options)
 };
