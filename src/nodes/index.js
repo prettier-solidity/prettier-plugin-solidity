f054d3ba54caae5228dff315bef1405354218d0b
Klaus Hott Vidal <klahott@gmail.com>
2019-06-11 10:56:23 +1200

    eslint you craycray

--- src/nodes/index.js
+++ src/nodes/index.js
@@ -1,61 +1,121 @@
+const ArrayTypeName = require('./ArrayTypeName.js');
+const AssemblyAssignment = require('./AssemblyAssignment.js');
+const AssemblyBlock = require('./AssemblyBlock.js');
+const AssemblyCall = require('./AssemblyCall.js');
+const AssemblyCase = require('./AssemblyCase.js');
+const AssemblyFor = require('./AssemblyFor.js');
+const AssemblyIf = require('./AssemblyIf.js');
+const AssemblyLocalDefinition = require('./AssemblyLocalDefinition.js');
+const AssemblySwitch = require('./AssemblySwitch.js');
+const BinaryOperation = require('./BinaryOperation.js');
+const Block = require('./Block.js');
+const BooleanLiteral = require('./BooleanLiteral.js');
+const BreakStatement = require('./BreakStatement.js');
+const Conditional = require('./Conditional.js');
+const ContinueStatement = require('./ContinueStatement.js');
+const ContractDefinition = require('./ContractDefinition.js');
+const DecimalNumber = require('./DecimalNumber.js');
+const ElementaryTypeName = require('./ElementaryTypeName.js');
+const ElementaryTypeNameExpression = require('./ElementaryTypeNameExpression.js');
+const EmitStatement = require('./EmitStatement.js');
+const EnumDefinition = require('./EnumDefinition.js');
+const EnumValue = require('./EnumValue.js');
+const EventDefinition = require('./EventDefinition.js');
+const ExpressionStatement = require('./ExpressionStatement.js');
+const ForStatement = require('./ForStatement.js');
+const FunctionCall = require('./FunctionCall.js');
+const FunctionDefinition = require('./FunctionDefinition.js');
+const FunctionTypeName = require('./FunctionTypeName.js');
+const HexLiteral = require('./HexLiteral.js');
+const HexNumber = require('./HexNumber.js');
+const Identifier = require('./Identifier.js');
+const IfStatement = require('./IfStatement.js');
+const ImportDirective = require('./ImportDirective.js');
+const IndexAccess = require('./IndexAccess.js');
+const InheritanceSpecifier = require('./InheritanceSpecifier.js');
+const InlineAssemblyStatement = require('./InlineAssemblyStatement.js');
+const LabelDefinition = require('./LabelDefinition.js');
+const Mapping = require('./Mapping.js');
+const MemberAccess = require('./MemberAccess.js');
+const ModifierDefinition = require('./ModifierDefinition.js');
+const ModifierInvocation = require('./ModifierInvocation.js');
+const NewExpression = require('./NewExpression.js');
+const NumberLiteral = require('./NumberLiteral.js');
+const Parameter = require('./Parameter.js');
+const ParameterList = require('./ParameterList.js');
+const PragmaDirective = require('./PragmaDirective.js');
+const ReturnStatement = require('./ReturnStatement.js');
+const SourceUnit = require('./SourceUnit.js');
+const StateVariableDeclaration = require('./StateVariableDeclaration.js');
+const StringLiteral = require('./StringLiteral.js');
+const StructDefinition = require('./StructDefinition.js');
+const ThrowStatement = require('./ThrowStatement.js');
+const TupleExpression = require('./TupleExpression.js');
+const UnaryOperation = require('./UnaryOperation.js');
+const UserDefinedTypeName = require('./UserDefinedTypeName.js');
+const UsingForDeclaration = require('./UsingForDeclaration.js');
+const VariableDeclaration = require('./VariableDeclaration.js');
+const VariableDeclarationStatement = require('./VariableDeclarationStatement.js');
+const WhileStatement = require('./WhileStatement.js');
+
 module.exports = {
-  ArrayTypeName: require('./ArrayTypeName.js'),
-  AssemblyAssignment: require('./AssemblyAssignment.js'),
-  AssemblyBlock: require('./AssemblyBlock.js'),
-  AssemblyCall: require('./AssemblyCall.js'),
-  AssemblyCase: require('./AssemblyCase.js'),
-  AssemblyFor: require('./AssemblyFor.js'),
-  AssemblyIf: require('./AssemblyIf.js'),
-  AssemblyLocalDefinition: require('./AssemblyLocalDefinition.js'),
-  AssemblySwitch: require('./AssemblySwitch.js'),
-  BinaryOperation: require('./BinaryOperation.js'),
-  Block: require('./Block.js'),
-  BooleanLiteral: require('./BooleanLiteral.js'),
-  BreakStatement: require('./BreakStatement.js'),
-  Conditional: require('./Conditional.js'),
-  ContinueStatement: require('./ContinueStatement.js'),
-  ContractDefinition: require('./ContractDefinition.js'),
-  DecimalNumber: require('./DecimalNumber.js'),
-  ElementaryTypeName: require('./ElementaryTypeName.js'),
-  ElementaryTypeNameExpression: require('./ElementaryTypeNameExpression.js'),
-  EmitStatement: require('./EmitStatement.js'),
-  EnumDefinition: require('./EnumDefinition.js'),
-  EnumValue: require('./EnumValue.js'),
-  EventDefinition: require('./EventDefinition.js'),
-  ExpressionStatement: require('./ExpressionStatement.js'),
-  ForStatement: require('./ForStatement.js'),
-  FunctionCall: require('./FunctionCall.js'),
-  FunctionDefinition: require('./FunctionDefinition.js'),
-  FunctionTypeName: require('./FunctionTypeName.js'),
-  HexLiteral: require('./HexLiteral.js'),
-  HexNumber: require('./HexNumber.js'),
-  Identifier: require('./Identifier.js'),
-  IfStatement: require('./IfStatement.js'),
-  ImportDirective: require('./ImportDirective.js'),
-  IndexAccess: require('./IndexAccess.js'),
-  InheritanceSpecifier: require('./InheritanceSpecifier.js'),
-  InlineAssemblyStatement: require('./InlineAssemblyStatement.js'),
-  LabelDefinition: require('./LabelDefinition.js'),
-  Mapping: require('./Mapping.js'),
-  MemberAccess: require('./MemberAccess.js'),
-  ModifierDefinition: require('./ModifierDefinition.js'),
-  ModifierInvocation: require('./ModifierInvocation.js'),
-  NewExpression: require('./NewExpression.js'),
-  NumberLiteral: require('./NumberLiteral.js'),
-  Parameter: require('./Parameter.js'),
-  ParameterList: require('./ParameterList.js'),
-  PragmaDirective: require('./PragmaDirective.js'),
-  ReturnStatement: require('./ReturnStatement.js'),
-  SourceUnit: require('./SourceUnit.js'),
-  StateVariableDeclaration: require('./StateVariableDeclaration.js'),
-  StringLiteral: require('./StringLiteral.js'),
-  StructDefinition: require('./StructDefinition.js'),
-  ThrowStatement: require('./ThrowStatement.js'),
-  TupleExpression: require('./TupleExpression.js'),
-  UnaryOperation: require('./UnaryOperation.js'),
-  UserDefinedTypeName: require('./UserDefinedTypeName.js'),
-  UsingForDeclaration: require('./UsingForDeclaration.js'),
-  VariableDeclaration: require('./VariableDeclaration.js'),
-  VariableDeclarationStatement: require('./VariableDeclarationStatement.js'),
-  WhileStatement: require('./WhileStatement.js')
+  ArrayTypeName,
+  AssemblyAssignment,
+  AssemblyBlock,
+  AssemblyCall,
+  AssemblyCase,
+  AssemblyFor,
+  AssemblyIf,
+  AssemblyLocalDefinition,
+  AssemblySwitch,
+  BinaryOperation,
+  Block,
+  BooleanLiteral,
+  BreakStatement,
+  Conditional,
+  ContinueStatement,
+  ContractDefinition,
+  DecimalNumber,
+  ElementaryTypeName,
+  ElementaryTypeNameExpression,
+  EmitStatement,
+  EnumDefinition,
+  EnumValue,
+  EventDefinition,
+  ExpressionStatement,
+  ForStatement,
+  FunctionCall,
+  FunctionDefinition,
+  FunctionTypeName,
+  HexLiteral,
+  HexNumber,
+  Identifier,
+  IfStatement,
+  ImportDirective,
+  IndexAccess,
+  InheritanceSpecifier,
+  InlineAssemblyStatement,
+  LabelDefinition,
+  Mapping,
+  MemberAccess,
+  ModifierDefinition,
+  ModifierInvocation,
+  NewExpression,
+  NumberLiteral,
+  Parameter,
+  ParameterList,
+  PragmaDirective,
+  ReturnStatement,
+  SourceUnit,
+  StateVariableDeclaration,
+  StringLiteral,
+  StructDefinition,
+  ThrowStatement,
+  TupleExpression,
+  UnaryOperation,
+  UserDefinedTypeName,
+  UsingForDeclaration,
+  VariableDeclaration,
+  VariableDeclarationStatement,
+  WhileStatement
 };
