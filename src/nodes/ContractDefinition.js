c7099f81a0c3f816cdf6c2e24c670e66a0c53b3f
Klaus Hott Vidal <klahott@gmail.com>
2019-06-11 17:05:14 +1200

    when having multiple base contracts we split the Base contracts and keep the `is` keyword in the first line. (#134)

--- src/nodes/ContractDefinition.js
+++ src/nodes/ContractDefinition.js
@@ -1,33 +1,53 @@
+/* eslint-disable implicit-arrow-linebreak */
 const {
   doc: {
-    builders: { concat, indent, join, line }
+    builders: { concat, group, indent, join, line }
   }
 } = require('prettier');
 const printPreservingEmptyLines = require('./print-preserving-empty-lines');
 
-const ContractDefinition = {
-  print: ({ node, options, path, print }) => {
-    let parts = [node.kind, ' ', node.name];
-
-    if (node.baseContracts.length > 0) {
-      parts = parts.concat([
-        ' is ',
-        join(', ', path.map(print, 'baseContracts'))
-      ]);
-    }
-
-    parts.push(' {');
-    if (node.subNodes.length > 0) {
-      parts = parts.concat([
-        indent(line),
-        indent(printPreservingEmptyLines(path, 'subNodes', options, print)),
-        line
-      ]);
-    }
-    parts.push('}');
-
-    return concat(parts);
+const inheritance = (node, path, print) => {
+  if (node.baseContracts.length > 0) {
+    return concat([
+      ' is',
+      indent(
+        concat([
+          line,
+          join(concat([',', line]), path.map(print, 'baseContracts'))
+        ])
+      )
+    ]);
   }
+  return '';
+};
+
+const body = (node, path, options, print) => {
+  if (node.subNodes.length > 0) {
+    return concat([
+      indent(line),
+      indent(printPreservingEmptyLines(path, 'subNodes', options, print)),
+      line
+    ]);
+  }
+  return '';
+};
+
+const ContractDefinition = {
+  print: ({ node, options, path, print }) =>
+    concat([
+      group(
+        concat([
+          node.kind,
+          ' ',
+          node.name,
+          inheritance(node, path, print),
+          line,
+          '{'
+        ])
+      ),
+      body(node, path, options, print),
+      '}'
+    ])
 };
 
 module.exports = ContractDefinition;
