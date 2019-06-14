c4c9747742c5d72ccefd8a48c662b55e87c0b853
Klaus Hott Vidal <klahott@gmail.com>
2019-06-14 15:05:43 +1200

    Fixing "Modifier-style base constructor call without arguments" compiling error

--- src/nodes/ModifierInvocation.js
+++ src/nodes/ModifierInvocation.js
@@ -1,17 +1,38 @@
 const {
   doc: {
-    builders: { concat, join }
+    builders: { concat, group, indent, join, line, softline }
   }
 } = require('prettier');
 
-const ModifierInvocation = {
-  print: ({ node, path, print }) => {
-    let doc = node.name;
-    if (node.arguments && node.arguments.length > 0) {
-      doc = concat([doc, '(', join(', ', path.map(print, 'arguments')), ')']);
-    }
-    return doc;
+const modifierArguments = (node, path, print, options) => {
+  if (node.arguments && node.arguments.length > 0) {
+    return group(
+      concat([
+        '(',
+        indent(
+          concat([
+            softline,
+            join(concat([',', line]), path.map(print, 'arguments'))
+          ])
+        ),
+        softline,
+        ')'
+      ])
+    );
   }
+  // TODO: remove Capital letter Hack once solidity-parser-antlr fixes this
+  if (
+    path.getParentNode().isConstructor && // if we are in a Constructor.
+    node.name[0] !== node.name[0].toLowerCase() // Modifier starts with a Capital letter.
+  ) {
+    return '()';
+  }
+  return '';
+};
+
+const ModifierInvocation = {
+  print: ({ node, path, print, options }) =>
+    concat([node.name, modifierArguments(node, path, print, options)])
 };
 
 module.exports = ModifierInvocation;
