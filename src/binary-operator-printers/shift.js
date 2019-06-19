34b39c4bd1ca1783e687ca1abb2fc112c25a5159
Klaus Hott Vidal <klahott@gmail.com>
2019-06-20 10:05:29 +1200

    remove duplicated code

--- src/binary-operator-printers/shift.js
+++ src/binary-operator-printers/shift.js
@@ -1,40 +1,6 @@
-const {
-  doc: {
-    builders: { group, line, concat, indent }
-  }
-} = require('prettier');
-const comparison = require('./comparison.js');
-
-const groupIfNecessaryBuilder = path => doc =>
-  path.getParentNode().type === 'BinaryOperation' ? doc : group(doc);
-
-const indentIfNecessaryBuilder = path => doc => {
-  const parentNode = path.getParentNode();
-  if (
-    parentNode.type === 'BinaryOperation' &&
-    !comparison.match(parentNode.operator)
-  ) {
-    return doc;
-  }
-  return indent(doc);
-};
+const arithmetic = require('./arithmetic.js');
 
 module.exports = {
   match: op => ['<<', '>>'].includes(op),
-  print: (node, path, print) => {
-    const groupIfNecessary = groupIfNecessaryBuilder(path);
-    const indentIfNecessary = indentIfNecessaryBuilder(path);
-
-    return groupIfNecessary(
-      indentIfNecessary(
-        concat([
-          path.call(print, 'left'),
-          ' ',
-          node.operator,
-          line,
-          path.call(print, 'right')
-        ])
-      )
-    );
-  }
+  print: arithmetic.print
 };
