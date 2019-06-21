70b766d08164e68ec07e3aac68ca5bfd585ea338
Klaus Hott Vidal <klahott@gmail.com>
2019-06-21 16:03:43 +1200

    The new version of solidity-parser-antlr doesn't have a Parameter list, it uses a collection of variable declarations.

--- src/nodes/EventDefinition.js
+++ src/nodes/EventDefinition.js
@@ -1,12 +1,29 @@
 const {
   doc: {
-    builders: { concat }
+    builders: { concat, group, indent, join, line, softline }
   }
 } = require('prettier/standalone');
 
+const parameters = (node, path, print) => {
+  if (node.parameters && node.parameters.length > 0) {
+    return group(
+      concat([
+        indent(
+          concat([
+            softline,
+            join(concat([',', line]), path.map(print, 'parameters'))
+          ])
+        ),
+        softline
+      ])
+    );
+  }
+  return '';
+};
+
 const EventDefinition = {
   print: ({ node, path, print }) =>
-    concat(['event ', node.name, '(', path.call(print, 'parameters'), ');'])
+    concat(['event ', node.name, '(', parameters(node, path, print), ');'])
 };
 
 module.exports = EventDefinition;
--- src/nodes/FunctionDefinition.js
+++ src/nodes/FunctionDefinition.js
@@ -1,6 +1,6 @@
 const {
   doc: {
-    builders: { concat, dedent, group, indent, join, line }
+    builders: { concat, dedent, group, indent, join, line, softline }
   }
 } = require('prettier/standalone');
 
@@ -8,6 +8,23 @@ const functionName = node => {
   if (node.isConstructor && !node.name) return 'constructor';
   if (node.name) return `function ${node.name}`;
   return 'function';
+};
+
+const parameters = (parametersType, node, path, print) => {
+  if (node[parametersType] && node[parametersType].length > 0) {
+    return group(
+      concat([
+        indent(
+          concat([
+            softline,
+            join(concat([',', line]), path.map(print, parametersType))
+          ])
+        ),
+        softline
+      ])
+    );
+  }
+  return '';
 };
 
 const visibility = node => {
@@ -36,7 +53,7 @@ const returnParameters = (node, path, print) => {
     return concat([
       line,
       'returns (',
-      path.call(print, 'returnParameters'),
+      parameters('returnParameters', node, path, print),
       ')'
     ]);
   }
@@ -58,7 +75,7 @@ const FunctionDefinition = {
     concat([
       functionName(node),
       '(',
-      path.call(print, 'parameters'),
+      parameters('parameters', node, path, print),
       ')',
       indent(
         group(
