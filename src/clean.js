1be22224462c32a195faeafcfe9af94c1ac43f73
Klaus Hott Vidal <klahott@gmail.com>
2019-06-27 15:34:40 +1200

    Now we are properly indenting BlockComments so the raw will have a different leading whitespace.

--- src/clean.js
+++ src/clean.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 function clean(ast, newObj, parent) {
-  ['code', 'codeStart', 'loc', 'range'].forEach(name => {
+  ['code', 'codeStart', 'loc', 'range', 'raw'].forEach(name => {
     delete newObj[name]; // eslint-disable-line no-param-reassign
   });
 }
