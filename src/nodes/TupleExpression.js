5710581a0d87abf3b8d0d713141599258aef3e65
Klaus Hott Vidal <klahott@gmail.com>
2019-08-05 12:25:00 -0400

    Explicit parentheses in operations where precedence might not be clear (#175)
    * parentheses are added to binary operations based on the rules of prettier js
    
    * adding default for eslint
    
    * only indent tuples if it's an array or multiple returns

--- src/nodes/TupleExpression.js
+++ src/nodes/TupleExpression.js
@@ -4,22 +4,35 @@ const {
   }
 } = require('prettier/standalone');
 
+const contents = (node, path, print) => {
+  if (
+    node.components &&
+    node.components.length === 1 &&
+    node.components[0].type === 'BinaryOperation'
+  ) {
+    return path.map(print, 'components');
+  }
+  return [
+    indent(
+      concat([
+        softline,
+        join(
+          concat([',', line]),
+          path.map(print, node.components ? 'components' : 'elements')
+        )
+      ])
+    ),
+    softline
+  ];
+};
+
 const TupleExpression = {
   // @TODO: remove hack once solidity-parser-antlr is fixed
   print: ({ node, path, print }) =>
     group(
       concat([
         node.isArray ? '[' : '(',
-        indent(
-          concat([
-            softline,
-            join(
-              concat([',', line]),
-              path.map(print, node.components ? 'components' : 'elements')
-            )
-          ])
-        ),
-        softline,
+        ...contents(node, path, print),
         node.isArray ? ']' : ')'
       ])
     )
