f9aa075b04e66066d83b9bcb38138202d342d10b
Jack Bates <jack@nottheoilrig.com>
2019-08-12 22:31:39 -0700

    Apply singleQuote option to hex literals (#190)

--- src/nodes/HexLiteral.js
+++ src/nodes/HexLiteral.js
@@ -1,5 +1,16 @@
+const {
+  doc: {
+    builders: { concat }
+  }
+} = require('prettier/standalone');
+
 const HexLiteral = {
-  print: ({ node }) => node.value
+  print: ({ node, options }) =>
+    concat(
+      options.singleQuote
+        ? ["hex'", node.value.slice(4, -1), "'"]
+        : ['hex"', node.value.slice(4, -1), '"']
+    )
 };
 
 module.exports = HexLiteral;
