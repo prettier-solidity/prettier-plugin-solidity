824021233e926a1c85b236ad926e7250150d40d9
Klaus Hott Vidal <klahott@gmail.com>
2019-09-19 11:58:08 +1200

    adding support for prettier-ignore

--- src/printer.js
+++ src/printer.js
@@ -1,4 +1,5 @@
 const nodes = require('./nodes');
+const { hasNodeIgnoreComment } = require('./prettier-comments/common/util.js');
 
 function genericPrint(path, options, print) {
   const node = path.getValue();
@@ -8,6 +9,10 @@ function genericPrint(path, options, print) {
 
   if (!(node.type in nodes)) {
     throw new Error(`Unknown type: ${JSON.stringify(node.type)}`);
+  }
+
+  if (hasNodeIgnoreComment(node)) {
+    return options.originalText.slice(node.range[0], node.range[1] + 1);
   }
 
   return nodes[node.type].print({ node, options, path, print });
