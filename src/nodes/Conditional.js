1b79aa691b3634687e2a596f410381e34c906a12
Klaus Hott Vidal <klahott@gmail.com>
2019-12-25 07:06:13 +1300

    true and false Expression need to be indented in case the line breaks (#209)
    * trueExpression and falseExpression need to be indented in case the line breaks
    
    * only 1 indent needed

--- src/nodes/Conditional.js
+++ src/nodes/Conditional.js
@@ -9,12 +9,16 @@ const Conditional = {
     group(
       concat([
         path.call(print, 'condition'),
-        indent(line),
-        '? ',
-        path.call(print, 'trueExpression'),
-        indent(line),
-        ': ',
-        path.call(print, 'falseExpression')
+        indent(
+          concat([
+            line,
+            '? ',
+            path.call(print, 'trueExpression'),
+            line,
+            ': ',
+            path.call(print, 'falseExpression')
+          ])
+        )
       ])
     )
 };
