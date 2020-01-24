46e50343c2eaaa7be7d0d4b8871a0c7d0e15073c
Klaus Hott Vidal <klahott@gmail.com>
2020-01-25 10:49:16 +1300

    Adding spaces between functions and contracts (#217)
    * Removing extra empty lines at the end of a block
    
    * Adding spaces between functions and contracts
    
    * adding scenario where function is followed by a non-function node

--- src/nodes/print-preserving-empty-lines.js
+++ src/nodes/print-preserving-empty-lines.js
@@ -8,20 +8,45 @@ const {
 function printPreservingEmptyLines(path, key, options, print) {
   const parts = [];
   path.each(childPath => {
+    const node = childPath.getValue();
+    const nodeType = node.type;
+
     if (parts.length !== 0) {
       parts.push(hardline);
     }
 
+    if (childPath.getName() > 0) {
+      if (nodeType === 'ContractDefinition') {
+        if (parts[parts.length - 2] !== hardline) {
+          parts.push(hardline);
+        }
+        parts.push(hardline);
+      }
+
+      if (
+        nodeType === 'FunctionDefinition' &&
+        parts[parts.length - 2] !== hardline
+      ) {
+        parts.push(hardline);
+      }
+    }
+
     parts.push(print(childPath));
+
     if (
       isNextLineEmptyAfterIndex(
         options.originalText,
-        options.locEnd(childPath.getValue()) + 1
-      )
+        options.locEnd(node) + 1
+      ) ||
+      nodeType === 'FunctionDefinition'
     ) {
       parts.push(hardline);
     }
   }, key);
+
+  if (parts.length > 1 && parts[parts.length - 1] === hardline) {
+    parts.pop();
+  }
 
   return concat(parts);
 }
