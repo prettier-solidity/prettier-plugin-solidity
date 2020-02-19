dceba4427c2efcddd77a58a024d066e3498be617
Klaus Hott Vidal <klahott@gmail.com>
2020-02-20 03:45:16 +1300

    ElementaryTypeName takes care of how to print state mutability (#229)
    * ElementaryTypeName takes care of how to print state mutability
    
    * small refactor to get rid of `filter(element => element)`

--- src/nodes/ElementaryTypeName.js
+++ src/nodes/ElementaryTypeName.js
@@ -1,5 +1,16 @@
+const {
+  doc: {
+    builders: { concat }
+  }
+} = require('prettier/standalone');
+
+const stateMutability = node =>
+  node.stateMutability && node.stateMutability.length > 0
+    ? concat([' ', node.stateMutability])
+    : '';
+
 const ElementaryTypeName = {
-  print: ({ node }) => node.name
+  print: ({ node }) => concat([node.name, stateMutability(node)])
 };
 
 module.exports = ElementaryTypeName;
--- src/nodes/VariableDeclaration.js
+++ src/nodes/VariableDeclaration.js
@@ -1,38 +1,39 @@
 const {
   doc: {
-    builders: { join }
+    builders: { concat }
   }
 } = require('prettier/standalone');
+
+const indexed = node => (node.isIndexed ? ' indexed' : '');
+
+const visibility = node =>
+  node.visibility && node.visibility !== 'default'
+    ? concat([' ', node.visibility])
+    : '';
+
+const constantKeyword = node => (node.isDeclaredConst ? ' constant' : '');
+
+const storageLocation = node =>
+  node.storageLocation && node.visibility !== 'default'
+    ? concat([' ', node.storageLocation])
+    : '';
+
+const name = node => (node.name ? concat([' ', node.name]) : '');
 
 const VariableDeclaration = {
   print: ({ node, path, print }) => {
     if (!node.typeName) {
       return node.name;
     }
-    let doc = path.call(print, 'typeName');
-    if (node.isIndexed) {
-      doc = join(' ', [doc, 'indexed']);
-    }
-    const constantKeyword = node.isDeclaredConst ? 'constant' : '';
-    if (node.visibility === 'default') {
-      return join(
-        ' ',
-        [doc, node.typeName.stateMutability, constantKeyword, node.name].filter(
-          element => element
-        )
-      );
-    }
-    return join(
-      ' ',
-      [
-        doc,
-        node.typeName.stateMutability,
-        node.visibility,
-        constantKeyword,
-        node.storageLocation,
-        node.name
-      ].filter(element => element)
-    );
+
+    return concat([
+      path.call(print, 'typeName'),
+      indexed(node),
+      visibility(node),
+      constantKeyword(node),
+      storageLocation(node),
+      name(node)
+    ]);
   }
 };
 
