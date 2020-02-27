4cd5412e0ca47776b91b33085f5b1e721eb4d3d9
Klaus Hott Vidal <klahott@gmail.com>
2020-02-28 08:12:55 +1300

    we check if the last character is a ')' to catch function calls without parameters (#239)

--- src/nodes/AssemblyCall.js
+++ src/nodes/AssemblyCall.js
@@ -7,8 +7,9 @@ const {
 const printList = require('./print-list');
 
 const AssemblyCall = {
-  print: ({ node, path, print }) =>
-    node.arguments.length === 0
+  print: ({ node, path, print, options }) =>
+    node.arguments.length === 0 &&
+    options.originalText.charAt(options.locEnd(node)) !== ')'
       ? node.functionName
       : concat([
           node.functionName,
