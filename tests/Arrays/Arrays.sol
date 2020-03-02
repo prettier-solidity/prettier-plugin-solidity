a4ae510d755add659bedbcb0143102fe68f776e7
Klaus Hott Vidal <klahott@gmail.com>
2020-03-03 12:43:42 +1300

    adding test described in #207

--- tests/Arrays/Arrays.sol
+++ tests/Arrays/Arrays.sol
@@ -2,4 +2,8 @@ pragma solidity ^0.5.2;
 
 contract Arrays {
     bytes32[] public permissions = [bytes32("addPartner"), bytes32("removePartner"), bytes32("addGroup"), bytes32("removeGroup"), bytes32("addUserToGroup"), bytes32("removeUserFromGroup"), bytes32("addSolution"), bytes32("removeSolution"), bytes32("addLibrary"), bytes32("removeLibrary"), bytes32("addPermissionToGroup"), bytes32("removePermissionFromGroup")];
+
+    function a() {
+    Outcome.OutcomeItem[] memory outcomeFrom = abi.decode(fromPart.outcome, (Outcome.OutcomeItem[]));
+}
 }
