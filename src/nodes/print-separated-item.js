4f512268a88e2dfb85fb9ea6ec3bb7f048f95823
Klaus Hott Vidal <klahott@gmail.com>
2020-03-06 11:18:10 +1300

    adding documentation for the new functions

--- src/nodes/print-separated-item.js
+++ src/nodes/print-separated-item.js
@@ -4,6 +4,11 @@ const {
   }
 } = require('prettier/standalone');
 
+// This function will add an indentation to the `item` and separate it from the
+// rest of the `doc` in most cases by a `softline`.
+//
+// NOTE: it doesn't `group` the resulting `doc` because single items can also be
+// part of a larger structure.
 const printSeparatedItem = (
   item,
   { firstSeparator = softline, lastSeparator = firstSeparator } = {}
--- src/nodes/print-separated-list.js
+++ src/nodes/print-separated-list.js
@@ -4,6 +4,13 @@ const {
   }
 } = require('prettier/standalone');
 
+// This function will add an indentation to the `list` and separate it from the
+// rest of the `doc` in most cases by a `softline`.
+// the list itself will be printed with a separator that in most cases is a
+// comma (,) and a `line`
+//
+// NOTE: the resulting `doc` in wrapped in a `group` because multiple items
+// are usually their own structure.
 const printSeparatedList = (
   list,
   {
