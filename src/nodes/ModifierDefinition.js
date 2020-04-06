1901453df49fca0eca5900f5d6742021d634235d
Klaus Hott Vidal <klahott@gmail.com>
2020-04-07 11:17:47 +1200

    Adding an extra rule for splitting params if the list is longer than 2 independent of the length on the line

--- src/nodes/ModifierDefinition.js
+++ src/nodes/ModifierDefinition.js
@@ -1,6 +1,6 @@
 const {
   doc: {
-    builders: { concat }
+    builders: { concat, hardline, line }
   }
 } = require('prettier/standalone');
 
@@ -9,7 +9,19 @@ const printSeparatedList = require('./print-separated-list');
 const modifierParameters = (node, path, print) => {
   if (node.parameters) {
     return node.parameters.length > 0
-      ? concat(['(', printSeparatedList(path.map(print, 'parameters')), ')'])
+      ? concat([
+          '(',
+          printSeparatedList(path.map(print, 'parameters'), {
+            separator: concat([
+              ',',
+              // To keep consistency any list of parameters will split if it's longer than 2.
+              // For more information see:
+              // https://github.com/prettier-solidity/prettier-plugin-solidity/issues/256
+              node.parameters.length > 2 ? hardline : line
+            ])
+          }),
+          ')'
+        ])
       : '()';
   }
 
