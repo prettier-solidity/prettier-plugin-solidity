33d76228a482e2aa09386b0bfef9008ecd956f65
mattia richetto <mattia.richetto@gmail.com>
2020-05-07 16:56:20 -0700

    s/C/Immutable

--- tests/Immutable/Immutable.sol
+++ tests/Immutable/Immutable.sol
@@ -1,6 +1,6 @@
 pragma solidity >0.6.4 <0.7.0;
 
-contract C {
+contract Immutable {
     uint constant X = 32**22 + 8;
     string constant TEXT = "abc";
     bytes32 constant MY_HASH = keccak256("abc");
