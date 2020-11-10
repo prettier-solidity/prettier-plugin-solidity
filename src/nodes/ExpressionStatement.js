4531e41005bed979dcec18237bb78171acc27804
Franco Victorio <victorio.franco@gmail.com>
2020-11-09 21:44:47 -0300

    Run prettier

--- src/nodes/ExpressionStatement.js
+++ src/nodes/ExpressionStatement.js
@@ -8,24 +8,24 @@ const printComments = require('./print-comments');
 
 const ExpressionStatement = {
   print: ({ node, options, path, print }) => {
-    const parts = []
+    const parts = [];
 
-    const parent = path.getParentNode()
+    const parent = path.getParentNode();
 
     if (parent.type === 'IfStatement') {
       if (node.comments && node.comments.length) {
-        const comments = printComments(node, path, options)
+        const comments = printComments(node, path, options);
         if (comments && comments.parts && comments.parts.length) {
-          parts.push(comments)
-          parts.push(hardline)
+          parts.push(comments);
+          parts.push(hardline);
         }
       }
     }
 
-    parts.push(path.call(print, 'expression'))
-    parts.push(node.omitSemicolon ? '' : ';')
+    parts.push(path.call(print, 'expression'));
+    parts.push(node.omitSemicolon ? '' : ';');
 
-    return concat(parts)
+    return concat(parts);
   }
 };
 
