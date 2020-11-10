aee4f5125c58da1459e86077c0290f10e69c18f9
Franco Victorio <victorio.franco@gmail.com>
2020-11-10 09:21:28 -0300

    Handle long state variable declarations

--- src/nodes/StateVariableDeclaration.js
+++ src/nodes/StateVariableDeclaration.js
@@ -1,11 +1,22 @@
 const {
   doc: {
-    builders: { concat }
+    builders: { concat, group, indent, line }
   }
 } = require('prettier/standalone');
 
-const initialValue = (node, path, print) =>
-  node.initialValue ? concat([' = ', path.call(print, 'initialValue')]) : '';
+const initialValue = (node, path, print) => {
+  if (!node.initialValue) {
+    return '';
+  }
+
+  if (node.initialValue.type === 'TupleExpression') {
+    return concat([' = ', path.call(print, 'initialValue')]);
+  }
+
+  return group(
+    concat([' =', indent(concat([line, path.call(print, 'initialValue')]))])
+  );
+};
 
 const StateVariableDeclaration = {
   print: ({ node, path, print }) =>
