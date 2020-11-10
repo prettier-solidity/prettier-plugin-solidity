c911fd0ac95f4c30474362f322e3de1d46ead8a5
Franco Victorio <victorio.franco@gmail.com>
2020-11-10 07:37:44 -0300

    Improve index access formatting

--- src/nodes/IndexAccess.js
+++ src/nodes/IndexAccess.js
@@ -1,12 +1,22 @@
 const {
   doc: {
-    builders: { concat }
+    builders: { concat, group, indent, softline }
   }
 } = require('prettier/standalone');
 
 const IndexAccess = {
   print: ({ path, print }) =>
-    concat([path.call(print, 'base'), '[', path.call(print, 'index'), ']'])
+    concat([
+      path.call(print, 'base'),
+      '[',
+      group(
+        concat([
+          indent(concat([softline, path.call(print, 'index')])),
+          softline
+        ])
+      ),
+      ']'
+    ])
 };
 
 module.exports = IndexAccess;
