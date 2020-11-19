a737175242825c3dc29f667095caadd3c695a368
Franco Victorio <victorio.franco@gmail.com>
2020-11-18 22:23:09 -0300

    Don't join multi part literals

--- src/nodes/HexLiteral.js
+++ src/nodes/HexLiteral.js
@@ -1,5 +1,16 @@
+const {
+  doc: {
+    builders: { join, line }
+  }
+} = require('prettier/standalone');
+
+const { printString } = require('../prettier-comments/common/util');
+
 const HexLiteral = {
-  print: ({ node }) => node.value
+  print: ({ node, options }) => {
+    const list = node.parts.map((part) => `hex${printString(part, options)}`);
+    return join(line, list);
+  }
 };
 
 module.exports = HexLiteral;
--- src/nodes/StringLiteral.js
+++ src/nodes/StringLiteral.js
@@ -1,7 +1,15 @@
+const {
+  doc: {
+    builders: { join, line }
+  }
+} = require('prettier/standalone');
 const { printString } = require('../prettier-comments/common/util');
 
 const StringLiteral = {
-  print: ({ node, options }) => printString(node.value, options)
+  print: ({ node, options }) => {
+    const list = node.parts.map((part) => printString(part, options));
+    return join(line, list);
+  }
 };
 
 module.exports = StringLiteral;
