e9aff327823e87b0feed1558c493845adee07a54
mattia richetto <mattia.richetto@gmail.com>
2020-12-07 12:39:39 -0800

    update eslint-config-prettier

--- src/nodes/FileLevelConstant.js
+++ src/nodes/FileLevelConstant.js
@@ -5,16 +5,15 @@ const {
 } = require('prettier/standalone');
 
 const FileLevelConstant = {
-  print: ({ node, path, print }) => {
-    return concat([
+  print: ({ node, path, print }) =>
+    concat([
       path.call(print, 'typeName'),
       ' constant ',
       node.name,
       ' = ',
       path.call(print, 'initialValue'),
       ';'
-    ]);
-  }
+    ])
 };
 
 module.exports = FileLevelConstant;
