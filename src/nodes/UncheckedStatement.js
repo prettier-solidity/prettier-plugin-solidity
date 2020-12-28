f29a28492ea281b692d52987129664783f7e34c7
Franco Victorio <victorio.franco@gmail.com>
2020-12-28 16:18:25 -0300

    Run prettier

--- src/nodes/UncheckedStatement.js
+++ src/nodes/UncheckedStatement.js
@@ -5,11 +5,7 @@ const {
 } = require('prettier/standalone');
 
 const WhileStatement = {
-  print: ({ path, print }) =>
-    concat([
-      'unchecked ',
-      path.call(print, 'block')
-    ])
+  print: ({ path, print }) => concat(['unchecked ', path.call(print, 'block')])
 };
 
 module.exports = WhileStatement;
