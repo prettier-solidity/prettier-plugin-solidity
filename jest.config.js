b26892eed4fc24309d1b738e7cf801483e7885b8
Franco Victorio <victorio.franco@gmail.com>
2021-03-09 13:59:46 -0300

    Upgrade parser, unicode strings, catch Panic (#444)
    * Handle catch clauses with Panic
    
    * Add support for unicode strings
    
    * Upgrade parser
    
    * Check coverage threshold on CI

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ module.exports = {
     '!src/prettier-comments/**/*.js'
   ],
   coverageDirectory: './coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   setupFiles: ['<rootDir>/tests_config/run_spec.js'],
   snapshotSerializers: ['<rootDir>/tests_config/raw-serializer.js'],
   testEnvironment: 'node',
--- src/nodes/StringLiteral.js
+++ src/nodes/StringLiteral.js
@@ -7,7 +7,15 @@ const { printString } = require('../prettier-comments/common/util');
 
 const StringLiteral = {
   print: ({ node, options }) => {
-    const list = node.parts.map((part) => printString(part, options));
+    const list = node.parts.map((part, index) =>
+      printString(part, {
+        ...options,
+        // node.isUnicode is an array of the same length as node.parts
+        // that indicates if that string fragment has the unicode prefix
+        isUnicode: node.isUnicode[index]
+      })
+    );
+
     return join(line, list);
   }
 };
