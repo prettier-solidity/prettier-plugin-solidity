ea738be310441fb4e991670f7dd400aec18c64d8
Klaus Hott Vidal <klahott@gmail.com>
2021-03-17 22:57:49 +1300

    Comments for an empty parameter list (#452)

--- src/nodes/print-comments.js
+++ src/nodes/print-comments.js
@@ -4,14 +4,17 @@ const {
   }
 } = require('prettier');
 
-const printComments = (node, path, options) =>
+const printComments = (node, path, options, filter = () => true) =>
   node.comments
     ? join(
         line,
         path
           .map((commentPath) => {
             const comment = commentPath.getValue();
-            if (comment.trailing || comment.leading) {
+            if (comment.trailing || comment.leading || comment.printed) {
+              return null;
+            }
+            if (!filter(comment)) {
               return null;
             }
             comment.printed = true;
