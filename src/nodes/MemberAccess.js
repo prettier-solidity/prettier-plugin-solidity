b9de41509b242496f94db6a72d3de72a092c88f7
Klaus Hott Vidal <klahott@gmail.com>
2021-05-30 13:45:17 +1200

    We indent only if the chain doesn't belong to a variable declaration or a binary operation (#516)

--- src/nodes/MemberAccess.js
+++ src/nodes/MemberAccess.js
@@ -16,11 +16,28 @@ const isBeginnigOfChain = (path) => {
   return true;
 };
 
+const shallIndent = (path) => {
+  let i = 0;
+  let elderNode = path.getParentNode(i);
+  while (elderNode) {
+    if (
+      elderNode.type === 'VariableDeclarationStatement' ||
+      elderNode.type === 'BinaryOperation'
+    )
+      return false;
+    i += 1;
+    elderNode = path.getParentNode(i);
+  }
+  return true;
+};
+
 const MemberAccess = {
   print: ({ node, path, print }) => {
     const doc = [
       path.call(print, 'expression'),
-      indent([softline, '.', node.memberName])
+      shallIndent(path)
+        ? indent([softline, '.', node.memberName])
+        : [softline, '.', node.memberName]
     ];
 
     return isBeginnigOfChain(path) ? group(doc) : doc;
