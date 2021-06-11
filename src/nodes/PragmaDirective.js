e18d6fb825689832862aa5029170df52da804c26
Klaus Hott <klahott@gmail.com>
2021-06-12 03:20:59 +1200

    visit each PragmaDirective on parse and infer the compiler

--- src/nodes/PragmaDirective.js
+++ src/nodes/PragmaDirective.js
@@ -1,18 +1,5 @@
-const semver = require('semver');
-
 const PragmaDirective = {
-  print: ({ node }) => {
-    // @TODO: remove hack once solidity-parser-antlr is fixed
-    let value = node.value
-      .replace(/([<>=])/g, ' $1')
-      .replace(/< =/g, '<=')
-      .replace(/> =/g, '>=')
-      .trim();
-    if (value.split(' ').length > 1) {
-      value = semver.validRange(value);
-    }
-    return ['pragma ', node.name, ' ', value, ';'];
-  }
+  print: ({ node }) => ['pragma ', node.name, ' ', node.value, ';']
 };
 
 module.exports = PragmaDirective;
--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,8 @@
 const extractComments = require('solidity-comments-extractor');
 // https://prettier.io/docs/en/plugins.html#parsers
 const parser = require('@solidity-parser/parser');
+const semver = require('semver');
+const solc = require('./solc.json');
 
 const tryHug = (node, operators) => {
   if (node.type === 'BinaryOperation' && operators.includes(node.operator))
@@ -12,9 +14,40 @@ const tryHug = (node, operators) => {
   return node;
 };
 
-function parse(text, parsers, options) {
+function parse(text, _parsers, options) {
+  let { compiler } = options;
+  let compilers = [...solc];
   const parsed = parser.parse(text, { loc: true, range: true });
   parsed.comments = extractComments(text);
+
+  parser.visit(parsed, {
+    PragmaDirective(ctx) {
+      // @TODO: remove hack once the retrieval of @solidity-parser/parser is fixed
+      ctx.value = ctx.value
+        .replace(/([<>=])/g, ' $1')
+        .replace(/< =/g, '<=')
+        .replace(/> =/g, '>=')
+        .trim();
+      if (ctx.value.split(' ').length > 1) {
+        ctx.value = semver.validRange(ctx.value);
+      }
+      if (ctx.name !== 'solidity') return;
+      if (!['latest', 'earliest'].includes(compiler)) return;
+      compilers = compilers.filter((version) =>
+        semver.satisfies(version.value, ctx.value)
+      );
+    }
+  });
+
+  if (compilers.length === 0) compilers = [...solc];
+  if (compiler === 'latest') compiler = compilers[0].value;
+  if (compiler === 'earliest') compiler = compilers.slice(-1)[0].value;
+
+  parser.visit(parsed, {
+    SourceUnit(ctx) {
+      ctx.compiler = compiler;
+    }
+  });
 
   parser.visit(parsed, {
     ForStatement(ctx) {
@@ -22,12 +55,18 @@ function parse(text, parsers, options) {
         ctx.initExpression.omitSemicolon = true;
       }
       ctx.loopExpression.omitSemicolon = true;
-    },
+    }
+  });
+
+  parser.visit(parsed, {
     HexLiteral(ctx) {
       ctx.value = options.singleQuote
         ? `hex'${ctx.value.slice(4, -1)}'`
         : `hex"${ctx.value.slice(4, -1)}"`;
-    },
+    }
+  });
+
+  parser.visit(parsed, {
     ElementaryTypeName(ctx) {
       // We avoid making changes for bytes1 since the type 'byte' was removed
       // in v0.8.0
@@ -42,7 +81,10 @@ function parse(text, parsers, options) {
         if (ctx.name === 'uint256') ctx.name = 'uint';
         if (ctx.name === 'int256') ctx.name = 'int';
       }
-    },
+    }
+  });
+
+  parser.visit(parsed, {
     BinaryOperation(ctx) {
       switch (ctx.operator) {
         case '+':
