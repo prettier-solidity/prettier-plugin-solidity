c02436fb87c77fab0ff7b0df871e3390e4d89607
Klaus Hott Vidal <klahott@gmail.com>
2021-07-09 08:06:03 +1200

    Respecting prettiers default options (#549)
    * having all options defined in our options object and making sure the defaults are being respected. See #375
    
    * not using `byte` anymore
    
    * No testing when the js format on StandAlone
    
    * Update options.js
    
    Co-authored-by: Franco Victorio <victorio.franco@gmail.com>

--- tests/config/require-standalone.js
+++ tests/config/require-standalone.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const prettier = require("prettier/standalone");
+const babelPlugin = require("prettier/parser-babel");
 const solidityPlugin = require("../../src/index");
 
 module.exports = {
   formatWithCursor(input, options) {
     const $$$options = {
       ...options,
-      plugins: [solidityPlugin, ...(options.plugins || [])],
+      plugins: [babelPlugin, solidityPlugin, ...(options.plugins || [])],
     };
     return prettier.formatWithCursor(input, $$$options);
   },
@@ -16,7 +17,7 @@ module.exports = {
     parse(input, options, massage) {
       const $$$options = {
         ...options,
-        plugins: [solidityPlugin, ...(options.plugins || [])],
+        plugins: [babelPlugin, solidityPlugin, ...(options.plugins || [])],
       };
       return prettier.__debug.parse(input, $$$options, massage);
     },
