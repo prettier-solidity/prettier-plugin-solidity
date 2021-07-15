ea7f8e2b424b666ec1bd779982ca766379f5d4e3
Klaus Hott <klahott@gmail.com>
2021-07-15 15:57:40 +1200

    IndexAccess can also be at the start of a chain and can also break.

--- src/nodes/IndexAccess.js
+++ src/nodes/IndexAccess.js
@@ -1,16 +1,28 @@
 const {
   doc: {
-    builders: { group, indent, softline }
+    builders: { group, indent, label, softline }
   }
 } = require('prettier');
 
+let indexAccessId = 0;
+
 const IndexAccess = {
-  print: ({ path, print }) => [
-    path.call(print, 'base'),
-    '[',
-    group([indent([softline, path.call(print, 'index')]), softline]),
-    ']'
-  ]
+  print: ({ path, print }) => {
+    const indexAccessLabel = {
+      type: 'IndexAccess',
+      groupId: `IndexAccess-${indexAccessId}`
+    };
+    indexAccessId += 1;
+
+    return label(JSON.stringify(indexAccessLabel), [
+      path.call(print, 'base'),
+      '[',
+      group([indent([softline, path.call(print, 'index')]), softline], {
+        id: indexAccessLabel.groupId
+      }),
+      ']'
+    ]);
+  }
 };
 
 module.exports = IndexAccess;
