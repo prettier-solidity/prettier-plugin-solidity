351da67be89cc2d8ae65a99f2c69b1e7d2a10ef1
Klaus Hott <klahott@gmail.com>
2021-11-04 13:19:01 +1300

    passing tests

--- tests/format/ExplicitVariableTypes/ExplicitVariableTypes.sol
+++ tests/format/ExplicitVariableTypes/ExplicitVariableTypes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
 contract VariableTypesMixed {
     uint256 public a;
--- tests/format/Parentheses/SubNoParentheses.sol
+++ tests/format/Parentheses/SubNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
 contract SubNoParentheses {
     function subAdd(uint256 a, uint256 b, uint256 c)
