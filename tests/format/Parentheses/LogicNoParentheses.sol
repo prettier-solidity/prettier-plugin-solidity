acc83e1c0107d5fcad614e59f585aeea1722d557
Klaus Hott <klahott@gmail.com>
2021-12-22 11:52:54 +1300

    passing the tests

--- tests/format/Parentheses/LogicNoParentheses.sol
+++ tests/format/Parentheses/LogicNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.11;
 
 contract LogicNoParentheses {
     function orOr(bool a, bool b, bool c) public pure returns (bool) {
