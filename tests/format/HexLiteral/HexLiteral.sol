cfa9d785f74d0694a10001d1f3b2ed516134a3cb
Klaus Hott <klahott@gmail.com>
2022-04-14 22:29:54 +1200

    Passing tests

--- tests/format/HexLiteral/HexLiteral.sol
+++ tests/format/HexLiteral/HexLiteral.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.13;
 
 contract HexLiteral {
   bytes8 hex1 = hex'DeadBeef';
--- tests/format/ModifierInvocations/ModifierInvocations.sol
+++ tests/format/ModifierInvocations/ModifierInvocations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.13;
 
 contract ModifierDefinitions {
   // We enforce the use of parentheses in modifiers without parameters.
