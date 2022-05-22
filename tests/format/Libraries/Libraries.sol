9f9df23fcee633f17d7be8fa4c3c0fe76a85c39e
Klaus Hott <klahott@gmail.com>
2022-05-22 14:37:12 -0400

    UsingForDeclaration with support for functions

---
+++ tests/format/Libraries/Libraries.sol
@@ -0,0 +1,10 @@
+  using   Lib    for uint;
+using Lib for
+    uint global;
+using { f} for uint;
+using {f } for uint global;
+using { a, very,     long, and, complex,     Function, list, with, long, names }     for
+    uint;
+using { a, very,
+        long, and, complex, Function,
+        list, with, long, names }       for uint global;
