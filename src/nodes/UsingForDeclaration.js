fc77fd6eff0c232d84ff50a147592a94b62e03d7
Klaus Hott Vidal <klahott@gmail.com>
2022-05-28 11:14:14 -0400

    using ... for ... supporting functions and global (#683)
    * using the latest parser
    
    * UsingForDeclaration with support for functions
    
    * removing unused TypeNameExpression

--- src/nodes/UsingForDeclaration.js
+++ src/nodes/UsingForDeclaration.js
@@ -1,10 +1,26 @@
+const {
+  doc: {
+    builders: { line, softline }
+  }
+} = require('prettier');
+
+const printSeparatedList = require('./print-separated-list');
+
 const UsingForDeclaration = {
-  print: ({ node, path, print }) => [
+  print: ({ node, path, print, options }) => [
     'using ',
-    node.libraryName,
+    node.functions && node.functions.length
+      ? [
+          '{',
+          printSeparatedList(node.functions, {
+            firstSeparator: options.bracketSpacing ? line : softline
+          }),
+          '}'
+        ]
+      : node.libraryName,
     ' for ',
     node.typeName ? path.call(print, 'typeName') : '*',
-    ';'
+    node.isGlobal ? ' global;' : ';'
   ]
 };
 
