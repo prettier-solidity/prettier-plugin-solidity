f016fa3213f0148614848939fdff1bec2ba7a0c9
freeton20 <doctor4891@gmail.com>
2022-06-18 15:58:08 +0300

    copyleft

--- src/nodes/PragmaDirective.js
+++ src/nodes/PragmaDirective.js
@@ -1,9 +1,16 @@
+const printSeparatedList = require('./print-separated-list');
+
+const nodeValue = (node, path, print) => {
+  if (typeof node.value === 'string') return node.value;
+  return printSeparatedList(path.map(print, 'value'));
+}
+
 const PragmaDirective = {
-  print: ({ node }) => [
-    'pragma ', 
-    node.name, 
+  print: ({ node, path, print }) => [
+    'pragma ',
+    node.name,
     (node.value === '') ? '' : ' ',
-    node.value, ';']
+    nodeValue(node, path, print), ';']
 };
 
 module.exports = PragmaDirective;
