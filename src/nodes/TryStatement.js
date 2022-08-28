bb75a6e5cbe9aa2d99da8bdf1ad028e2a39353a1
freeton20 <doctor4891@gmail.com>
2022-08-28 13:07:41 +0300

    fix try-catch

--- src/nodes/TryStatement.js
+++ src/nodes/TryStatement.js
@@ -4,33 +4,34 @@ const {
   }
 } = require('prettier');
 
-const printSeparatedItem = require('./print-separated-item');
-const printSeparatedList = require('./print-separated-list');
+// const printSeparatedItem = require('./print-separated-item');
+// const printSeparatedList = require('./print-separated-list');
 
-const returnParameters = (node, path, print) =>
-  node.returnParameters
-    ? [
-        'returns (',
-        printSeparatedList(path.map(print, 'returnParameters')),
-        ')'
-      ]
-    : '';
+// const returnParameters = (node, path, print) =>
+//   node.returnParameters
+//     ? [
+//         'returns (',
+//         printSeparatedList(path.map(print, 'returnParameters')),
+//         ')'
+//       ]
+//     : '';
 
 const TryStatement = {
   print: ({ node, path, print }) => {
     let parts = [
       'try',
-      group(
-        printSeparatedItem(path.call(print, 'expression'), {
-          firstSeparator: line
-        })
-      )
+      ' ',
+      // group(
+      //   printSeparatedItem(path.call(print, 'expression'), {
+      //     firstSeparator: line
+      //   })
+      // )
     ];
 
-    const formattedReturnParameters = returnParameters(node, path, print);
-    if (formattedReturnParameters) {
-      parts = parts.concat([formattedReturnParameters, ' ']);
-    }
+    // const formattedReturnParameters = returnParameters(node, path, print);
+    // if (formattedReturnParameters) {
+    //   parts = parts.concat([formattedReturnParameters, ' ']);
+    // }
 
     parts = parts.concat([
       path.call(print, 'body'),
