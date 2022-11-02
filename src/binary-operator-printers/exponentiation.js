b41272a23bd1b315a5e9ea145af2028549518d54
Klaus Hott <klahott@gmail.com>
2022-11-02 14:45:42 +1300

    passing the linter

--- src/binary-operator-printers/exponentiation.js
+++ src/binary-operator-printers/exponentiation.js
@@ -1,18 +1,13 @@
 const {
   doc: {
-    builders: { group, ifBreak, indent, line }
+    builders: { group, indent, line }
   }
 } = require('prettier');
 
 module.exports = {
   match: (op) => op === '**',
   print: (node, path, print) => {
-    const right = [
-      ' ',
-      node.operator,
-      line,
-      path.call(print, 'right')
-    ];
+    const right = [' ', node.operator, line, path.call(print, 'right')];
     // If it's a single binary operation, avoid having a small right
     // operand like - 1 on its own line
     const shouldGroup =
