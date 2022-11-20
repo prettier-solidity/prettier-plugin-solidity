d6d04a6e981ef662d8a44891dbbd7f7fa2306556
Klaus Hott <klahott@gmail.com>
2022-11-20 17:01:09 +1300

    removing TODO

--- src/clean.js
+++ src/clean.js
@@ -1,6 +1,4 @@
-// Prettier now offers a clean way to define ignored properties.
-// TODO: for some reason clean doesn't iterate over comments. Either ignore
-// them completely or add logic to iterate over them in the clean function.
+// Prettier offers a clean way to define ignored properties.
 const ignoredProperties = new Set(['loc', 'range', 'comments']);
 function clean(/* ast, newObj, parent */) {}
 clean.ignoredProperties = ignoredProperties;
