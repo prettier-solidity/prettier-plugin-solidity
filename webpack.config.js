f52f375512cf690c5471b7660387d53c8ba4bd31
Franco Victorio <victorio.franco@gmail.com>
2022-12-13 18:43:06 +0100

    Use source-map instead of cheap-module-source-map

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (webpackEnv) => {
 
     mode: isEnvProduction ? 'production' : 'development',
     bail: isEnvProduction,
-    devtool: 'cheap-module-source-map',
+    devtool: 'source-map',
 
     // We tell webpack to use the browser friendly package.
     resolve: {
