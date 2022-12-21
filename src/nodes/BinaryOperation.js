ee7ab518d1ce801c1ab03a363d984ce33cce1910
Klaus Hott <klahott@gmail.com>
2022-12-22 11:57:22 +1300

    better error handling

--- src/nodes/BinaryOperation.js
+++ src/nodes/BinaryOperation.js
@@ -2,10 +2,19 @@
 const printers = require('../binary-operator-printers');
 
 const BinaryOperation = {
-  print: ({ node, path, print, options }) =>
-    Object.values(printers)
-      .find((printer) => printer.match(node.operator))
-      .print(node, path, print, options)
+  print: ({ node, path, print, options }) => {
+    const binaryOperationPrinter = Object.values(printers).find((printer) =>
+      printer.match(node.operator)
+    );
+    /* c8 ignore start */
+    if (binaryOperationPrinter === undefined) {
+      throw new Error(
+        `Assertion error: no printer found for operator ${node.operator}`
+      );
+    }
+    /* c8 ignore stop */
+    return binaryOperationPrinter.print(node, path, print, options);
+  }
 };
 
 module.exports = BinaryOperation;
