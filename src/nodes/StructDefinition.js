ce7134fc9e0fb1792d9405157ea2f1857c0c4a86
Klaus Hott <klahott@gmail.com>
2023-04-24 17:20:52 +0300

    Refactor StructDefinition

--- src/nodes/StructDefinition.js
+++ src/nodes/StructDefinition.js
@@ -7,23 +7,19 @@ const {
 const { printSeparatedList } = require('../common/printer-helpers');
 
 const StructDefinition = {
-  print: ({ node, path, print }) => {
-    const parts = ['struct ', node.name, ' {'];
-
-    if (node.members.length > 0) {
-      parts.push(
-        printSeparatedList(path.map(print, 'members'), {
+  print: ({ node, path, print }) => [
+    'struct ',
+    node.name,
+    ' {',
+    node.members.length > 0
+      ? printSeparatedList(path.map(print, 'members'), {
           firstSeparator: hardline,
           separator: [';', hardline],
           lastSeparator: [';', hardline]
         })
-      );
-    }
-
-    parts.push('}');
-
-    return parts;
-  }
+      : '',
+    '}'
+  ]
 };
 
 module.exports = StructDefinition;
