eaa366e7591160177565dbc191765115b80db9da
Klaus Hott <klahott@gmail.com>
2023-07-19 18:01:41 +1200

    unit tests to ESM

--- tests/unit/comments/printer.test.js
+++ tests/unit/comments/printer.test.js
@@ -1,4 +1,4 @@
-const printComment = require('../../../src/comments/printer');
+import { printComment } from '../../../src/comments/printer.js';
 
 test('given an unknown comment type then printComment function should throw', () => {
   const mockCommentPath = { getValue: () => ({ type: 'UnknownComment' }) };
--- tests/unit/prettier-version.test.js
+++ tests/unit/prettier-version.test.js
@@ -1,23 +1,22 @@
-const { TEST_STANDALONE } = process.env;
+// eslint-disable-next-line import/no-extraneous-dependencies
+import proxyquire from 'proxyquire';
+import getPrettier from '../config/get-prettier.js';
 
-const prettier = TEST_STANDALONE
-  ? require('prettier/standalone')
-  : require('prettier');
+// mocking is really difficult in ESM
+test.skip('should throw if the installed version of prettier is less than v2.3.0', async () => {
+  const prettier = await getPrettier();
 
-const proxyquire = require('proxyquire');
+  const prettierMock = proxyquire('../config/require-prettier', {
+    prettier: { ...prettier, version: '2.2.1', '@global': true }
+  });
 
-const prettierMock = proxyquire('../config/require-prettier', {
-  prettier: { ...prettier, version: '2.2.1', '@global': true }
-});
+  const plugin = proxyquire(
+    process.env.TEST_STANDALONE ? '../../dist/standalone' : '../../src/index',
+    {
+      prettier: prettierMock
+    }
+  );
 
-const plugin = proxyquire(
-  TEST_STANDALONE ? '../../dist/standalone' : '../../src/index',
-  {
-    prettier: prettierMock
-  }
-);
-
-test('should throw if the installed version of prettier is less than v2.3.0', async () => {
   const data = 'contract CheckPrettierVersion {}';
 
   const options = {
