f3055dd1d3024721d3e3938f3cfcd9c78d7ffad8
Klaus Hott <klahott@gmail.com>
2023-11-04 14:13:38 +1300

    disabling linter rule

--- tests/integration/test-app.js
+++ tests/integration/test-app.js
@@ -1,4 +1,5 @@
 import prettier from 'prettier/standalone.js';
+// eslint-disable-next-line import/no-unresolved
 import solidityPlugin from '../../dist/standalone.cjs';
 
 export default async function format(code) {
