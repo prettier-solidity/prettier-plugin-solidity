452089be7d033c09ee08bb8082ea0355e8f77e98
Klaus Hott <klahott@gmail.com>
2023-11-19 21:57:45 +1300

    tuning up the breaking rules

--- src/nodes/Conditional.js
+++ src/nodes/Conditional.js
@@ -7,52 +7,60 @@ let groupIndex = 0;
 const experimentalTernaries = (node, path, print) => {
   const parent = path.getParentNode();
 
-  const hasConditionalAtEnd = node.falseExpression.type === 'Conditional';
+  const falseExpressionIsConditional =
+    node.falseExpression.type === 'Conditional';
   const hasConditional =
-    [node.condition.type, node.trueExpression.type].includes('Conditional') ||
-    hasConditionalAtEnd;
+    node.trueExpression.type === 'Conditional' || falseExpressionIsConditional;
 
-  const conditionalGroup = group([path.call(print, 'condition'), ' ?'], {
-    id: `Conditional.condition-${groupIndex}`
-  });
+  const conditionalGroup = group(
+    [
+      parent.type === 'Conditional' && parent.trueExpression === node
+        ? hardlineWithoutBreakParent
+        : '',
+      path.call(print, 'condition'),
+      ' ?'
+    ],
+    {
+      id: `Conditional.condition-${groupIndex}`
+    }
+  );
 
   groupIndex += 1;
+
   const expressionSeparator = ifBreak(hardlineWithoutBreakParent, line, {
     groupId: conditionalGroup.id
   });
 
   const document = group([
+    conditionalGroup,
+    group(indent([expressionSeparator, path.call(print, 'trueExpression')])),
     group([
-      conditionalGroup,
-      group(indent([expressionSeparator, path.call(print, 'trueExpression')]))
-    ]),
-    group([
-      parent.type === 'Conditional' || hasConditionalAtEnd ?
-        hardlineWithoutBreakParent
-      : expressionSeparator,
+      parent.type === 'Conditional' || falseExpressionIsConditional
+        ? hardlineWithoutBreakParent
+        : expressionSeparator,
       ': ',
       path.call(print, 'falseExpression')
     ])
   ]);
 
-  return path.parent.type === 'VariableDeclarationStatement' ?
-      ifBreak(indent([hasConditional ? hardline : line, document]), document)
+  return parent.type === 'VariableDeclarationStatement'
+    ? ifBreak(indent([hasConditional ? hardline : line, document]), document)
     : document;
 };
 
 export const Conditional = {
   print: ({ node, path, print, options }) =>
-    options.experimentalTernaries ?
-      experimentalTernaries(node, path, print)
-    : group([
-        path.call(print, 'condition'),
-        indent([
-          line,
-          '? ',
-          path.call(print, 'trueExpression'),
-          line,
-          ': ',
-          path.call(print, 'falseExpression')
+    options.experimentalTernaries
+      ? experimentalTernaries(node, path, print)
+      : group([
+          path.call(print, 'condition'),
+          indent([
+            line,
+            '? ',
+            path.call(print, 'trueExpression'),
+            line,
+            ': ',
+            path.call(print, 'falseExpression')
+          ])
         ])
-      ])
 };
