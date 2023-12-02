8e6caa1e59704aa02ca62439a83eae0a1a2bdeac
Klaus <klahott@gmail.com>
2023-12-02 09:13:47 -0300

    undoing unnecessary change

--- src/nodes/TupleExpression.js
+++ src/nodes/TupleExpression.js
@@ -3,29 +3,17 @@ import { printSeparatedList } from '../common/printer-helpers.js';
 
 const { group } = doc.builders;
 
-const contents = (node, path, print, options) => {
-  if (
-    options.experimentalTernaries &&
-    path.getParentNode().type === 'Conditional'
-  ) {
-    return printSeparatedList(path.map(print, 'components'));
-  }
-
-  return node.components &&
-    node.components.length === 1 &&
-    node.components[0].type === 'BinaryOperation'
+const contents = (node, path, print) =>
+  node.components &&
+  node.components.length === 1 &&
+  node.components[0].type === 'BinaryOperation'
     ? path.map(print, 'components')
     : printSeparatedList(path.map(print, 'components'));
-};
 
 export const TupleExpression = {
-  print: ({ node, path, print, options }) => {
+  print: ({ node, path, print }) => {
     const [openingBracket, closingBracket] = node.isArray ? '[]' : '()';
 
-    return group([
-      openingBracket,
-      contents(node, path, print, options),
-      closingBracket
-    ]);
+    return group([openingBracket, contents(node, path, print), closingBracket]);
   }
 };
