12ed51db406d04e8559f946e1e62deb8b24ef21e
Klaus <klahott@gmail.com>
2023-12-02 13:43:02 -0300

    group reorganisation that allows us to get rid of ifBreaks and hardlineWithoutBreakParent cleaning the resulting code

--- src/nodes/Conditional.js
+++ src/nodes/Conditional.js
@@ -1,10 +1,8 @@
 import { doc } from 'prettier';
 import { printSeparatedItem } from '../common/printer-helpers.js';
 
-const { group, hardline, hardlineWithoutBreakParent, ifBreak, indent, line } =
-  doc.builders;
+const { group, hardline, ifBreak, indent, line } = doc.builders;
 
-let groupIndex = 0;
 const experimentalTernaries = (node, path, print) => {
   const parent = path.getParentNode();
   const isNested = parent.type === 'Conditional';
@@ -16,49 +14,31 @@ const experimentalTernaries = (node, path, print) => {
   // If the `conditionDoc` breaks into multiple lines, we add parentheses,
   // unless it already is a `TupleExpression`.
   const conditionDoc = path.call(print, 'condition');
-  const conditionGroup = group(
-    [
-      node.condition.type === 'TupleExpression'
-        ? conditionDoc
-        : ifBreak(['(', printSeparatedItem(conditionDoc), ')'], conditionDoc),
-      ' ?'
-    ],
-    { id: `Conditional.condition-${groupIndex}` }
-  );
-
-  groupIndex += 1;
-
-  // If the `conditionGroup` breaks we force a new line to separate the
-  // `trueExpression` and `falseExpression` following the "curious" ternaries
-  // format.
-  // We need a `hardlineWithoutBreakParent` because `hardline` doesn't play
-  // well with `ifBreak`. (See https://github.com/prettier/prettier/issues/15710)
-  const expressionSeparator = ifBreak(hardlineWithoutBreakParent, line, {
-    groupId: conditionGroup.id
-  });
+  const conditionGroup = group([
+    node.condition.type === 'TupleExpression'
+      ? conditionDoc
+      : ifBreak(['(', printSeparatedItem(conditionDoc), ')'], conditionDoc),
+    ' ?'
+  ]);
 
   // To switch between "case-style" and "curious" ternaries we force a
   // `hardline` without propagating the break thus keeping "case-style"
   // ternaries in the parent `Conditional`s.
-  const trueExpressionDoc = printSeparatedItem(
-    [
-      isNestedAsTrueExpression ? hardline : expressionSeparator,
-      path.call(print, 'trueExpression')
-    ],
-    { firstSeparator: '' }
-  );
-
-  // We force a new line if current `Conditional` is nested or nests a
-  // `Conditional`. Otherwise we add a normal separator.
-  const falseExpressionDoc = group([
-    isNested || hasNestedConditional ? hardline : expressionSeparator,
-    ': ',
-    path.call(print, 'falseExpression')
+  const trueExpressionDoc = indent([
+    isNestedAsTrueExpression ? hardline : line,
+    path.call(print, 'trueExpression')
   ]);
 
+  // We force a new line if current `Conditional` is nested or nests a
+  // `Conditional`. Otherwise we add a normal line.
+  const falseExpressionDoc = [
+    isNested || hasNestedConditional ? hardline : line,
+    ': ',
+    path.call(print, 'falseExpression')
+  ];
+
   const document = group([
-    conditionGroup,
-    trueExpressionDoc,
+    group([conditionGroup, trueExpressionDoc]),
     falseExpressionDoc
   ]);
 
