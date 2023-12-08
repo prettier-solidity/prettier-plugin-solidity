28ea1b196a8372a6db6d03c2cc7fbcd30f15054b
Klaus <klahott@gmail.com>
2023-12-08 15:39:02 -0300

    this group always breaks

--- src/nodes/UncheckedStatement.js
+++ src/nodes/UncheckedStatement.js
@@ -1,7 +1,3 @@
-import { doc } from 'prettier';
-
-const { group } = doc.builders;
-
 export const UncheckedStatement = {
-  print: ({ path, print }) => group(['unchecked ', path.call(print, 'block')])
+  print: ({ path, print }) => ['unchecked ', path.call(print, 'block')]
 };
