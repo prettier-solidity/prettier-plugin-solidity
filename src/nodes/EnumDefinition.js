608be45b16b5889b797dd67830e7363cda329f15
Klaus <klahott@gmail.com>
2023-12-08 15:33:18 -0300

    this group always breaks

--- src/nodes/EnumDefinition.js
+++ src/nodes/EnumDefinition.js
@@ -1,17 +1,16 @@
 import { doc } from 'prettier';
 import { printSeparatedList } from '../common/printer-helpers.js';
 
-const { group, hardline } = doc.builders;
+const { hardline } = doc.builders;
 
 export const EnumDefinition = {
-  print: ({ node, path, print }) =>
-    group([
-      'enum ',
-      node.name,
-      ' {',
-      printSeparatedList(path.map(print, 'members'), {
-        firstSeparator: hardline
-      }),
-      '}'
-    ])
+  print: ({ node, path, print }) => [
+    'enum ',
+    node.name,
+    ' {',
+    printSeparatedList(path.map(print, 'members'), {
+      firstSeparator: hardline
+    }),
+    '}'
+  ]
 };
