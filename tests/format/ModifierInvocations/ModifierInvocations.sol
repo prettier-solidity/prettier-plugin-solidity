12ef526bb55651e5d7fe2629eca2b5484cf1cbe2
Klaus <klahott@gmail.com>
2024-03-17 23:10:20 +1300

    passing tests

--- tests/format/ModifierInvocations/ModifierInvocations.sol
+++ tests/format/ModifierInvocations/ModifierInvocations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 contract ModifierDefinitions {
   // We enforce the use of parentheses in modifiers without parameters.
--- tests/format/Parentheses/LogicNoParentheses.sol
+++ tests/format/Parentheses/LogicNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 contract LogicNoParentheses {
     function orOr(bool a, bool b, bool c) public pure returns (bool) {
