39780e4cbf7f2730a17593ff278acd2fedfe7c8f
yudrywet <166895665+yudrywet@users.noreply.github.com>
2024-04-15 07:29:54 +0800

    chore: remove repetitive words (#999)

--- tests/integration/node.test.js
+++ tests/integration/node.test.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 import format from '../../dist/test.cjs';
 
-test('Should run a a test app in a node environment', async () => {
+test('Should run a test app in a node environment', async () => {
   const data = 'contract    CheckPackage {}';
 
   expect(await format(data)).toEqual('contract CheckPackage {}\n');
