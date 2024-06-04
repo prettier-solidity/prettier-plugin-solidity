c84ad19b7856ad44aba5403a428bb26722e2eb92
Klaus <klahott@gmail.com>
2024-06-04 13:36:36 +1200

    Member Access Chains

--- src/slang-nodes/FunctionCallExpression.js
+++ src/slang-nodes/FunctionCallExpression.js
@@ -1,11 +1,36 @@
+import { doc } from 'prettier';
+import { isLabel } from '../common/util.js';
+
+const { group, indentIfBreak, label } = doc.builders;
+
+let groupIndex = 0;
 export const FunctionCallExpression = {
   parse: ({ ast, options, parse }) => ({
     kind: ast.cst.kind,
     operand: parse(ast.operand, options, parse),
     arguments: parse(ast.arguments, options, parse)
   }),
-  print: ({ path, print }) => [
-    path.call(print, 'operand'),
-    path.call(print, 'arguments')
-  ]
+  print: ({ path, print }) => {
+    let operandDoc = path.call(print, 'operand');
+    let argumentsDoc = path.call(print, 'arguments');
+
+    // If we are at the end of a MemberAccessChain we should indent the
+    // arguments accordingly.
+    if (isLabel(operandDoc) && operandDoc.label === 'MemberAccessChain') {
+      operandDoc = group(operandDoc.contents, {
+        id: `FunctionCall.expression-${groupIndex}`
+      });
+
+      groupIndex += 1;
+
+      argumentsDoc = indentIfBreak(argumentsDoc, {
+        groupId: operandDoc.id
+      });
+      // We wrap the expression in a label in case there is an IndexAccess or
+      // a FunctionCall following this IndexAccess.
+      return label('MemberAccessChain', [operandDoc, argumentsDoc]);
+    }
+
+    return [operandDoc, argumentsDoc];
+  }
 };
--- src/slang-nodes/IndexAccessExpression.js
+++ src/slang-nodes/IndexAccessExpression.js
@@ -1,3 +1,9 @@
+import { doc } from 'prettier';
+import { isLabel } from '../common/util.js';
+
+const { group, indent, indentIfBreak, label, softline } = doc.builders;
+
+let groupIndex = 0;
 export const IndexAccessExpression = {
   parse: ({ ast, options, parse }) => ({
     kind: ast.cst.kind,
@@ -7,11 +13,36 @@ export const IndexAccessExpression = {
     end: ast.end ? parse(ast.end, options, parse) : undefined,
     closeBracket: ast.closeBracket.text
   }),
-  print: ({ node, path, print }) => [
-    path.call(print, 'operand'),
-    node.openBracket,
-    node.start ? path.call(print, 'start') : '',
-    node.end ? path.call(print, 'end') : '',
-    node.closeBracket
-  ]
+  print: ({ node, path, print }) => {
+    let operandDoc = path.call(print, 'operand');
+    let indexDoc = group([
+      node.openBracket,
+      indent([
+        softline,
+        node.start ? path.call(print, 'start') : '',
+        node.end ? path.call(print, 'end') : ''
+      ]),
+      softline,
+      node.closeBracket
+    ]);
+
+    // If we are at the end of a MemberAccessChain we should indent the
+    // arguments accordingly.
+    if (isLabel(operandDoc) && operandDoc.label === 'MemberAccessChain') {
+      operandDoc = group(operandDoc.contents, {
+        id: `IndexAccess.base-${groupIndex}`
+      });
+
+      groupIndex += 1;
+
+      indexDoc = indentIfBreak(indexDoc, {
+        groupId: operandDoc.id
+      });
+      // We wrap the expression in a label in case there is an IndexAccess or
+      // a FunctionCall following this IndexAccess.
+      return label('MemberAccessChain', [operandDoc, indexDoc]);
+    }
+
+    return [operandDoc, indexDoc];
+  }
 };
