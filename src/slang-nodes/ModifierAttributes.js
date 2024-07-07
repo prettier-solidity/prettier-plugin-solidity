c11d1d0aec7b333faea025b71825bc0ec8a3bc75
Klaus <klahott@gmail.com>
2024-07-07 22:40:43 +1200

    sorting function attributes

--- src/slang-nodes/ModifierAttributes.js
+++ src/slang-nodes/ModifierAttributes.js
@@ -1,10 +1,13 @@
 import { doc } from 'prettier';
+import { sortFunctionAttributes } from '../common/slang-helpers.js';
 
 const { line } = doc.builders;
 
 export const ModifierAttributes = {
   parse: ({ offsets, ast, options, parse }) => ({
-    items: ast.items.map((item) => parse(item, options, parse, offsets))
+    items: ast.items
+      .map((item) => parse(item, options, parse, offsets))
+      .sort(sortFunctionAttributes)
   }),
   print: ({ path, print }) =>
     path.map(print, 'items').map((item) => [line, item])
