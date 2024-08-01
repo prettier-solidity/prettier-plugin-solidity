f0dadd59de68f9f585be560beeb1afcc03d0a740
Klaus <klahott@gmail.com>
2024-08-02 11:51:07 +1200

    using supported languages instead of magic constant '0..8.26'

--- src/clean.ts
+++ src/clean.ts
@@ -1,5 +1,6 @@
 // Prettier offers a clean way to define ignored properties.
 const ignoredProperties = new Set(['loc', 'range', 'comments']);
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 function clean(/* ast, newObj, parent */): void {}
 clean.ignoredProperties = ignoredProperties;
 
