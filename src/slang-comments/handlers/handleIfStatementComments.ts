45779304be043075ea6c111f98a35200ed687ff5
Klaus <klahott@gmail.com>
2024-08-02 15:24:56 +1200

    more if statement comment cases

--- src/slang-comments/handlers/handleIfStatementComments.ts
+++ src/slang-comments/handlers/handleIfStatementComments.ts
@@ -1,11 +1,12 @@
 import { NonterminalKind } from '@nomicfoundation/slang/kinds/index.js';
 import { util } from 'prettier';
 import { getNextNonSpaceNonCommentCharacter } from '../../slang-utils/backward-compatibility.js';
+import addHubNodeFirstComment from './add-hub-node-first-comment.js';
 import addHubNodeLastComment from './add-hub-node-last-comment.js';
 
 import type { HandlerParams } from './types.js';
 
-const { addTrailingComment } = util;
+const { addLeadingComment, addTrailingComment } = util;
 
 export default function handleIfStatementComments({
   text,
@@ -45,5 +46,25 @@ export default function handleIfStatementComments({
     return true;
   }
 
+  if (followingNode.kind === NonterminalKind.IfStatement) {
+    if (followingNode.body.variant.kind === NonterminalKind.Block) {
+      addHubNodeFirstComment(followingNode.body.variant.statements, comment);
+    } else {
+      addLeadingComment(followingNode.body.variant, comment);
+    }
+    return true;
+  }
+
+  // For comments positioned after the condition parenthesis in an if statement
+  // before the consequent without brackets on, such as
+  // if (a) /* comment */ true
+  if (enclosingNode.body === followingNode) {
+    if (followingNode.variant.kind === NonterminalKind.Block) {
+      addHubNodeFirstComment(followingNode.variant.statements, comment);
+    } else {
+      addLeadingComment(followingNode, comment);
+    }
+    return true;
+  }
   return false;
 }
