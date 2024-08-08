9a6fdeaf7e02313142c3c432192c0d6979c31284
Klaus <klahott@gmail.com>
2024-08-08 14:39:23 +1200

    solidity-parse's stack assignment now matches slang's

--- src/nodes/AssemblyStackAssignment.js
+++ src/nodes/AssemblyStackAssignment.js
@@ -1,7 +1,12 @@
+import { doc } from 'prettier';
+
+const { hardline } = doc.builders;
+
 export const AssemblyStackAssignment = {
   print: ({ node, path, print }) => [
     path.call(print, 'expression'),
-    ' =: ',
+    hardline,
+    '=: ',
     node.name
   ]
 };
