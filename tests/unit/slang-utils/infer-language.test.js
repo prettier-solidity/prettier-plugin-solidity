7fcf8e80c113d6b95f51b2e4d2d1521d34118b4c
Klaus <klahott@gmail.com>
2024-09-28 15:07:41 +1200

    adding more specific tests

--- tests/unit/slang-utils/infer-language.test.js
+++ tests/unit/slang-utils/infer-language.test.js
@@ -2,6 +2,10 @@ import { Language } from '@nomicfoundation/slang/language/index.js';
 import { inferLanguage } from '../../../src/slang-utils/infer-language.js';
 
 describe('inferLanguage', function () {
+  const supportedVersions = Language.supportedVersions();
+  const latestSupportedVersion =
+    supportedVersions[supportedVersions.length - 1];
+
   const fixtures = [
     {
       description: 'Caret range',
@@ -12,6 +16,11 @@ describe('inferLanguage', function () {
       description: 'Pinned version',
       source: `pragma solidity 0.8.1;`,
       version: '0.8.1'
+    },
+    {
+      description: 'With nightly commit',
+      source: `pragma solidity 0.8.18-ci.2023.1.17+commit.e7b959af;`,
+      version: '0.8.18'
     },
     {
       description: 'Caret range and pinned version',
@@ -26,6 +35,16 @@ describe('inferLanguage', function () {
     {
       description: 'With natspec comment before the range',
       source: `pragma solidity /** comment */ 0.8.2;`,
+      version: '0.8.2'
+    },
+    {
+      description: 'With multiline comment between the ranges',
+      source: `pragma solidity ^0.8.0 /* comment */ 0.8.2;`,
+      version: '0.8.2'
+    },
+    {
+      description: 'With natspec comment between the ranges',
+      source: `pragma solidity ^0.8.0 /** comment */ 0.8.2;`,
       version: '0.8.2'
     },
     {
@@ -52,20 +71,17 @@ describe('inferLanguage', function () {
     });
   }
 
-  test('should use the latest valid version if the source has no pragmas', function () {
-    const supportedVersions = Language.supportedVersions();
-    const latestSupportedVersion =
-      supportedVersions[supportedVersions.length - 1];
-    let inferredLanguage = inferLanguage(`contract Foo {}`);
+  test('should use the latest version if the source has no pragmas', function () {
+    const inferredLanguage = inferLanguage(`contract Foo {}`);
     expect(inferredLanguage.version).toEqual(latestSupportedVersion);
-    inferredLanguage = inferLanguage(`contract Foo {byte bar;}`);
+  });
+
+  test('should use the latest valid version if the source has no pragmas and the syntax is old', function () {
+    const inferredLanguage = inferLanguage(`contract Foo {byte bar;}`);
     expect(inferredLanguage.version).toEqual('0.7.6');
   });
 
   test('should use the latest version if the range is outside the supported versions', function () {
-    const supportedVersions = Language.supportedVersions();
-    const latestSupportedVersion =
-      supportedVersions[supportedVersions.length - 1];
     const inferredLanguage = inferLanguage(`pragma solidity ^0.8.27;`);
     expect(inferredLanguage.version).toEqual(latestSupportedVersion);
   });
