209064d7e32a69af7085e8d6066f4e3a4d73e999
Klaus <klahott@gmail.com>
2024-10-21 21:07:43 +1300

    solc tests

--- tests/format/ModifierInvocations/ModifierInvocations.sol
+++ tests/format/ModifierInvocations/ModifierInvocations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 
 contract ModifierDefinitions {
   // We enforce the use of parentheses in modifiers without parameters.
--- tests/format/Parentheses/MulNoParentheses.sol
+++ tests/format/Parentheses/MulNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.28;
 
 contract MulNoParentheses {
     function mulAdd(uint256 a, uint256 b, uint256 c)
