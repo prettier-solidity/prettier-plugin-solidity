14039047606319237cdc1734d2bac0cb0ff7353e
Klaus <klahott@gmail.com>
2024-12-11 13:28:43 +1300

    using the new parser for ImportDirective tests

--- tests/format/ImportDirective/format.test.js
+++ tests/format/ImportDirective/format.test.js
@@ -1,4 +1,7 @@
-runFormatTest(import.meta, ['slang-solidity']);
-runFormatTest(import.meta, ['slang-solidity'], { bracketSpacing: true });
+runFormatTest(import.meta, ['slang-solidity'], { compiler: '0.8.28' });
+runFormatTest(import.meta, ['slang-solidity'], {
+  bracketSpacing: true,
+  compiler: '0.8.28'
+});
 runFormatTest(import.meta, ['solidity-parse']);
 runFormatTest(import.meta, ['solidity-parse'], { bracketSpacing: true });
