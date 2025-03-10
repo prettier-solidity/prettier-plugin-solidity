bf4df000e093ac63e23e0b8d8aac2135f984e18d
Klaus <klahott@gmail.com>
2025-03-10 22:19:19 +1300

    use the same logic for grouping leftover operators in case of an assignment

--- src/binary-operator-printers/comparison.js
+++ src/binary-operator-printers/comparison.js
@@ -24,15 +24,9 @@ export const comparison = {
   print: (node, path, print, options) => {
     const indentIfNecessary = indentIfNecessaryBuilder(path);
 
-    const right = rightOperandPrinter(node, path, print, options);
-    // If it's a single binary operation, avoid having a small right
-    // operand like - 1 on its own line
-    const shouldGroup =
-      node.left.type !== 'BinaryOperation' &&
-      path.getParentNode().type !== 'BinaryOperation';
     return group([
       path.call(print, 'left'),
-      indentIfNecessary(shouldGroup ? group(right) : right)
+      indentIfNecessary(rightOperandPrinter(node, path, print, options))
     ]);
   }
 };
--- src/binary-operator-printers/printers/right-operand-printer.js
+++ src/binary-operator-printers/printers/right-operand-printer.js
@@ -1,8 +1,19 @@
 import { doc } from 'prettier';
+import { assignment } from '../assignment.js';
 
-const { line } = doc.builders;
+const { group, line } = doc.builders;
 
-export const rightOperandPrinter = (node, path, print, options) =>
-  options.experimentalOperatorPosition === 'end'
-    ? [' ', node.operator, line, path.call(print, 'right')]
-    : [line, node.operator, ' ', path.call(print, 'right')];
+export const rightOperandPrinter = (node, path, print, options) => {
+  const right =
+    options.experimentalOperatorPosition === 'end'
+      ? [' ', node.operator, line, path.call(print, 'right')]
+      : [line, node.operator, ' ', path.call(print, 'right')];
+
+  // If it's a single binary operation, avoid having a small right
+  // operand like - 1 on its own line
+  const parent = path.getParentNode();
+  return node.left.type !== 'BinaryOperation' &&
+    (parent.type !== 'BinaryOperation' || assignment.match(parent.operator))
+    ? group(right)
+    : right;
+};
