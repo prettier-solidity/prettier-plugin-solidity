2e3127fb0b5d62ab421e2857fd1d258e564962d5
Klaus <klahott@gmail.com>
2025-03-15 13:21:28 +1300

    upon review, binary operation's indentation behave as `equality`, `inequality` when it's a child of `IfStatement`, `ForStatement`, or `WhileStatement` so there's no need for separate behaviours

--- src/binary-operator-printers/equality.js
+++ src/binary-operator-printers/equality.js
@@ -1,7 +1,7 @@
-import { comparisonOperationPrinter } from './printers/comparison-operation-printer.js';
+import { binaryOperationPrinter } from './printers/binary-operation-printer.js';
 import { logical } from './logical.js';
 
 export const equality = {
   match: (op) => ['==', '!='].includes(op),
-  print: comparisonOperationPrinter([logical])
+  print: binaryOperationPrinter([logical])
 };
--- src/binary-operator-printers/printers/binary-operation-printer.js
+++ src/binary-operator-printers/printers/binary-operation-printer.js
@@ -2,11 +2,8 @@ import { createBinaryOperationPrinter } from './create-binary-operation-printer.
 import { createGroupIfNecessaryBuilder } from './create-group-if-necessary-builder.js';
 import { createIndentIfNecessaryBuilder } from './create-indent-if-necessary-builder.js';
 
-export const createBinaryIndentIfNecessaryBuilder =
-  createIndentIfNecessaryBuilder(['ReturnStatement']);
-
 export const binaryOperationPrinter = (shouldGroupAndIndentMatchers) =>
   createBinaryOperationPrinter(
     createGroupIfNecessaryBuilder(shouldGroupAndIndentMatchers),
-    createBinaryIndentIfNecessaryBuilder(shouldGroupAndIndentMatchers)
+    createIndentIfNecessaryBuilder(shouldGroupAndIndentMatchers)
   );
