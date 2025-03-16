6c095eaeedd61bbada22f0b587831da1848befd1
Klaus <klahott@gmail.com>
2025-03-16 14:31:03 +1300

    rightOperandPrinter is only used in a single place thus it's not needed to be exported

--- src/binary-operator-printers/printers/create-binary-operation-printer.js
+++ src/binary-operator-printers/printers/create-binary-operation-printer.js
@@ -1,4 +1,22 @@
-import { rightOperandPrinter } from './right-operand-printer.js';
+import { doc } from 'prettier';
+import { assignment } from '../assignment.js';
+
+const { group, line } = doc.builders;
+
+const rightOperandPrinter = (node, path, print, options) => {
+  const right =
+    options.experimentalOperatorPosition === 'end'
+      ? [' ', node.operator, line, path.call(print, 'right')]
+      : [line, node.operator, ' ', path.call(print, 'right')];
+
+  // If it's a single binary operation, avoid having a small right
+  // operand like - 1 on its own line
+  const parent = path.getParentNode();
+  return node.left.type !== 'BinaryOperation' &&
+    (parent.type !== 'BinaryOperation' || assignment.match(parent.operator))
+    ? group(right)
+    : right;
+};
 
 export const createBinaryOperationPrinter =
   (groupIfNecessaryBuilder, indentIfNecessaryBuilder) =>
