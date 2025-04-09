d078f99638793b5704975f507523b9ef93448051
Klaus <klahott@gmail.com>
2025-04-09 15:05:48 +1200

    unused files

--- src/slang-utils/prettier-version-satisfies.ts
+++
@@ -1,8 +0,0 @@
-import { version } from 'prettier';
-import satisfies from 'semver/functions/satisfies.js';
-
-import type { Range } from 'semver';
-
-export function prettierVersionSatisfies(range: string | Range): boolean {
-  return satisfies(version, range);
-}
