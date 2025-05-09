81f2db350ae35413b7eb48f8010141ede1e9d057
Klaus <klahott@gmail.com>
2025-05-09 14:05:54 +0200

    passing tests

--- tests/format/HexLiteral/HexLiteral.sol
+++ tests/format/HexLiteral/HexLiteral.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.29;
+pragma solidity 0.8.30;
 
 contract HexLiteral {
   bytes8 hex1 = hex'DeadBeef';
--- tests/format/Parentheses/MulNoParentheses.sol
+++ tests/format/Parentheses/MulNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.29;
+pragma solidity 0.8.30;
 
 contract MulNoParentheses {
     function mulAdd(uint256 a, uint256 b, uint256 c)
