da4f26401aef057efae8f5f24ab4d17ff9dad414
Klaus <klahott@gmail.com>
2025-06-02 20:01:26 +0200

    Using Slang's language inference tool

--- src/slang-utils/create-parser.ts
+++ src/slang-utils/create-parser.ts
@@ -1,16 +1,7 @@
-import { VersionExpressionSets as SlangVersionExpressionSets } from '@nomicfoundation/slang/ast';
-import { NonterminalKind, Query } from '@nomicfoundation/slang/cst';
+import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { Parser } from '@nomicfoundation/slang/parser';
 import { LanguageFacts } from '@nomicfoundation/slang/utils';
-import {
-  maxSatisfying,
-  minSatisfying,
-  minor,
-  major,
-  satisfies,
-  validRange
-} from 'semver';
-import { VersionExpressionSets } from '../slang-nodes/VersionExpressionSets.js';
+import { maxSatisfying } from 'semver';
 
 import type { ParseOutput } from '@nomicfoundation/slang/parser';
 import type { ParserOptions } from 'prettier';
@@ -18,128 +9,71 @@ import type { AstNode } from '../slang-nodes/types.js';
 
 const supportedVersions = LanguageFacts.allVersions();
 
-const milestoneVersions = Array.from(
-  supportedVersions.reduce((minorRanges, version) => {
-    minorRanges.add(`^${major(version)}.${minor(version)}.0`);
-    return minorRanges;
-  }, new Set<string>())
-)
-  .reverse()
-  .reduce((versions: string[], range) => {
-    versions.push(maxSatisfying(supportedVersions, range)!);
-    versions.push(minSatisfying(supportedVersions, range)!);
-    return versions;
-  }, []);
-
-const queries = [
-  Query.create('[VersionPragma @versionRanges [VersionExpressionSets]]')
-];
-
-let parser: Parser;
+// This list was retrieved from Slang's documentation.
+// https://nomicfoundation.github.io/slang/latest/solidity-grammar/supported-versions/
+// TODO: use the run-time functionality if NomicFoundation decides to expose
+// this information directly.
+const milestoneVersions = [
+  '0.4.14',
+  '0.4.16',
+  '0.4.21',
+  '0.4.22',
+  '0.4.25',
+  '0.5.0',
+  '0.5.3',
+  '0.5.5',
+  '0.5.8',
+  '0.5.10',
+  '0.5.14',
+  '0.6.0',
+  '0.6.2',
+  '0.6.5',
+  '0.6.7',
+  '0.6.8',
+  '0.6.11',
+  '0.7.0',
+  '0.7.1',
+  '0.7.4',
+  '0.8.0',
+  '0.8.4',
+  '0.8.8',
+  '0.8.13',
+  '0.8.18',
+  '0.8.19',
+  '0.8.22',
+  '0.8.27',
+  '0.8.29'
+].map((milestone) => maxSatisfying(supportedVersions, `<${milestone}`)!);
 
 export function createParser(
   text: string,
   options: ParserOptions<AstNode>
 ): [Parser, ParseOutput] {
+  let parser: Parser;
   const compiler = maxSatisfying(supportedVersions, options.compiler);
   if (compiler) {
-    if (!parser || parser.languageVersion !== compiler) {
-      parser = Parser.create(compiler);
-    }
+    parser = Parser.create(compiler);
     return [parser, parser.parseNonterminal(NonterminalKind.SourceUnit, text)];
   }
 
-  let isCachedParser = false;
-  if (parser) {
-    isCachedParser = true;
-  } else {
-    parser = Parser.create(milestoneVersions[0]);
-  }
-
   let parseOutput;
-  let inferredRanges: string[] = [];
 
-  try {
-    parseOutput = parser.parseNonterminal(NonterminalKind.SourceUnit, text);
-    inferredRanges = tryToCollectPragmas(parseOutput, parser, isCachedParser);
-  } catch {
-    for (
-      let i = isCachedParser ? 0 : 1;
-      i <= milestoneVersions.length;
-      i += 1
-    ) {
-      try {
-        const version = milestoneVersions[i];
-        parser = Parser.create(version);
-        parseOutput = parser.parseNonterminal(NonterminalKind.SourceUnit, text);
-        inferredRanges = tryToCollectPragmas(parseOutput, parser);
-        break;
-      } catch {
-        continue;
-      }
-    }
-  }
+  const inferredRanges: string[] = LanguageFacts.inferLanguageVersions(text);
+  parser = Parser.create(inferredRanges[inferredRanges.length - 1]);
+  parseOutput = parser.parseNonterminal(NonterminalKind.SourceUnit, text);
 
-  const satisfyingVersions = inferredRanges.reduce(
-    (versions, inferredRange) => {
-      if (!validRange(inferredRange)) {
-        throw new Error(
-          `Couldn't infer any version from the ranges in the pragmas${options.filepath ? ` for file ${options.filepath}` : ''}`
-        );
-      }
-      return versions.filter((supportedVersion) =>
-        satisfies(supportedVersion, inferredRange)
-      );
-    },
-    supportedVersions
+  if (parseOutput.isValid()) return [parser, parseOutput];
+
+  const inferredMilestones = milestoneVersions.filter((milestone) =>
+    inferredRanges.includes(milestone)
   );
 
-  const inferredVersion =
-    satisfyingVersions.length > 0
-      ? satisfyingVersions[satisfyingVersions.length - 1]
-      : supportedVersions[supportedVersions.length - 1];
-
-  if (inferredVersion !== parser.languageVersion) {
-    parser = Parser.create(inferredVersion);
+  for (let i = inferredMilestones.length - 1; i > 0; i -= 1) {
+    const version = inferredMilestones[i];
+    parser = Parser.create(version);
     parseOutput = parser.parseNonterminal(NonterminalKind.SourceUnit, text);
+    if (parseOutput.isValid()) break;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  return [parser, parseOutput!];
-}
-
-function tryToCollectPragmas(
-  parseOutput: ParseOutput,
-  parser: Parser,
-  isCachedParser = false
-): string[] {
-  const matches = parseOutput.createTreeCursor().query(queries);
-  const ranges: string[] = [];
-
-  let match;
-  while ((match = matches.next())) {
-    const versionRange = new SlangVersionExpressionSets(
-      match.captures.versionRanges[0].node.asNonterminalNode()!
-    );
-    ranges.push(
-      // Replace all comments that could be in the expression with whitespace
-      new VersionExpressionSets(versionRange).comments.reduce(
-        (range, comment) => range.replace(comment.value, ' '),
-        versionRange.cst.unparse()
-      )
-    );
-  }
-
-  if (ranges.length === 0) {
-    // If we didn't find pragmas but succeeded parsing the source we keep it.
-    if (!isCachedParser && parseOutput.isValid()) {
-      return [parser.languageVersion];
-    }
-    // Otherwise we throw.
-    throw new Error(
-      `Using version ${parser.languageVersion} did not find any pragma statement and does not parse without errors.`
-    );
-  }
-
-  return ranges;
+  return [parser, parseOutput];
 }
