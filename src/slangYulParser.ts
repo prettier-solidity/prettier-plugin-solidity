8bbe539a9aa680362d04fec7bd6ba59554a5dcc2
Klaus <klahott@gmail.com>
2025-06-18 10:59:54 +0100

    using createParser to infer the correct Language Version

--- src/slangYulParser.ts
+++ src/slangYulParser.ts
@@ -1,10 +1,7 @@
 // https://prettier.io/docs/en/plugins.html#parsers
 import { YulBlock as SlangYulBlock } from '@nomicfoundation/slang/ast';
-import { NonterminalKind } from '@nomicfoundation/slang/cst';
-import { Parser } from '@nomicfoundation/slang/parser';
-import { LanguageFacts } from '@nomicfoundation/slang/utils';
-import { coerce } from 'semver';
-import { clearOffsets } from './slang-utils/metadata.js';
+import { clearOffsets } from './slang-nodes/SlangNode.js';
+import { createParser } from './slang-utils/create-parser.js';
 import { YulBlock } from './slang-nodes/YulBlock.js';
 
 import type { ParserOptions } from 'prettier';
@@ -14,27 +11,14 @@ export default function parse(
   text: string,
   options: ParserOptions<AstNode>
 ): AstNode {
-  // const [parser, parseOutput] = createParser(text, options);
-  const compiler = coerce(options.compiler);
-  const supportedVersions = LanguageFacts.allVersions();
+  const { parser, parseOutput } = createParser(text, options);
 
-  const parser = Parser.create(
-    compiler && supportedVersions.includes(compiler.version)
-      ? compiler.version
-      : supportedVersions[supportedVersions.length - 1]
+  // We update the compiler version by the inferred one.
+  options.compiler = parser.languageVersion;
+  const parsed = new YulBlock(
+    new SlangYulBlock(parseOutput.tree.asNonterminalNode()),
+    options
   );
-
-  const parseOutput = parser.parseNonterminal(NonterminalKind.YulBlock, text);
-
-  if (parseOutput.isValid()) {
-    // We update the compiler version by the inferred one.
-    options.compiler = parser.languageVersion;
-    const parsed = new YulBlock(
-      new SlangYulBlock(parseOutput.tree.asNonterminalNode()),
-      options
-    );
-    clearOffsets();
-    return parsed;
-  }
-  throw new Error(parseOutput.errors()[0].message);
+  clearOffsets();
+  return parsed;
 }
