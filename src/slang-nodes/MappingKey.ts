281d98db21486fbe77867d6fb51c307d27a86d56
Klaus <klahott@gmail.com>
2025-07-10 22:39:58 +0100

    reordering the imports

--- src/slang-nodes/MappingKey.ts
+++ src/slang-nodes/MappingKey.ts
@@ -1,6 +1,6 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
-import { SlangNode } from './SlangNode.js';
 import { joinExisting } from '../slang-utils/join-existing.js';
+import { SlangNode } from './SlangNode.js';
 import { MappingKeyType } from './MappingKeyType.js';
 import { Identifier } from './Identifier.js';
 
