b306219cdc1185fc46e121032e3872bdd93c9b92
Klaus <klahott@gmail.com>
2025-07-16 20:39:19 +0100

    cleaning up types

--- src/slang-printers/print-variant.ts
+++ src/slang-printers/print-variant.ts
@@ -1,19 +1,22 @@
 import type { AstPath, Doc } from 'prettier';
 import type { PrintFunction } from '../types.d.ts';
-import type { AstNode, PolymorphicNode } from '../slang-nodes/types.js';
+import type { StrictPolymorphicNode } from '../slang-nodes/types.js';
 
 // TODO: Create a type that contains all `StrictAstNode`s that contain at least
-// a property that is a `PolymorphicNode | undefined`.
+// a property that is a `StrictPolymorphicNode | undefined`.
 export function printVariant(
   property: string,
   path: AstPath,
   print: PrintFunction
 ): Doc {
-  return path.call((polymorphicPath: AstPath<AstNode>) => {
-    const node = polymorphicPath.node as PolymorphicNode | undefined;
-    if (!node) return '';
-    return node.comments.length === 0
-      ? polymorphicPath.call(print, 'variant')
-      : print(polymorphicPath);
-  }, property);
+  return path.call(
+    (polymorphicPath: AstPath<StrictPolymorphicNode | undefined>) => {
+      const node = polymorphicPath.node;
+      if (!node) return '';
+      return node.comments.length === 0
+        ? polymorphicPath.call(print, 'variant')
+        : print(polymorphicPath);
+    },
+    property
+  );
 }
