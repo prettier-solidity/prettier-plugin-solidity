e3d1afae964de0e6be64963e46c2f7e295e0172e
Klaus <klahott@gmail.com>
2025-07-22 14:10:17 +0100

    standardising `printPreservingEmptyLines`

--- src/slang-printers/print-preserving-empty-lines.ts
+++ src/slang-printers/print-preserving-empty-lines.ts
@@ -1,6 +1,7 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { doc, util } from 'prettier';
 import { locEnd } from '../slang-utils/loc.js';
+import { printComments } from './print-comments.js';
 
 import type { AstPath, Doc, ParserOptions } from 'prettier';
 import type { AstNode, LineCollection } from '../slang-nodes/types.d.ts';
@@ -13,25 +14,28 @@ export function printPreservingEmptyLines(
   print: PrintFunction,
   options: ParserOptions<AstNode>
 ): Doc {
-  return path.map((childPath) => {
-    const node = childPath.node;
+  return [
+    path.map((childPath) => {
+      const node = childPath.node;
 
-    return [
-      // Only attempt to prepend an empty line if `node` is not the first item
-      !childPath.isFirst &&
-      // YulLabel adds a dedented line so we don't have to prepend a hardline.
-      (node.kind !== NonterminalKind.YulStatement ||
-        node.variant.kind !== NonterminalKind.YulLabel)
-        ? hardline
-        : '',
-      print(childPath),
-      // Only attempt to append an empty line if `node` is not the last item
-      !childPath.isLast &&
-      // Append an empty line if the original text already had an one after the
-      // current `node`
-      util.isNextLineEmpty(options.originalText, locEnd(node))
-        ? hardline
-        : ''
-    ];
-  }, 'items');
+      return [
+        // Only attempt to prepend an empty line if `node` is not the first item
+        !childPath.isFirst &&
+        // YulLabel adds a dedented line so we don't have to prepend a hardline.
+        (node.kind !== NonterminalKind.YulStatement ||
+          node.variant.kind !== NonterminalKind.YulLabel)
+          ? hardline
+          : '',
+        print(childPath),
+        // Only attempt to append an empty line if `node` is not the last item
+        !childPath.isLast &&
+        // Append an empty line if the original text already had an one after the
+        // current `node`
+        util.isNextLineEmpty(options.originalText, locEnd(node))
+          ? hardline
+          : ''
+      ];
+    }, 'items'),
+    printComments(path)
+  ];
 }
---
+++ tests/format/Empty/EmptyFileWithComments.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * TODO: Write contract.
+ */
