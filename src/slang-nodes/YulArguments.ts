fde97a761334a83ae950a2e42f5cc7211e74d3b2
Klaus <klahott@gmail.com>
2025-09-17 02:33:16 +0300

    removing `YulExpression` from the AST tree

--- src/slang-nodes/YulArguments.ts
+++ src/slang-nodes/YulArguments.ts
@@ -1,6 +1,6 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { printSeparatedList } from '../slang-printers/print-separated-list.js';
-import { printVariant } from '../slang-printers/print-variant.js';
+import { extractVariant } from '../slang-utils/extract-variant.js';
 import { SlangNode } from './SlangNode.js';
 import { YulExpression } from './YulExpression.js';
 
@@ -12,15 +12,17 @@ import type { AstNode } from './types.d.ts';
 export class YulArguments extends SlangNode {
   readonly kind = NonterminalKind.YulArguments;
 
-  items: YulExpression[];
+  items: YulExpression['variant'][];
 
   constructor(ast: ast.YulArguments, options: ParserOptions<AstNode>) {
     super(ast, true);
 
-    this.items = ast.items.map((item) => new YulExpression(item, options));
+    this.items = ast.items.map((item) =>
+      extractVariant(new YulExpression(item, options))
+    );
   }
 
   print(path: AstPath<YulArguments>, print: PrintFunction): Doc {
-    return printSeparatedList(path.map(printVariant(print), 'items'));
+    return printSeparatedList(path.map(print, 'items'));
   }
 }
