cbe36adcbe26a9d463c8162cf74693eb98d8bbc9
Klaus <klahott@gmail.com>
2026-01-30 15:56:47 -0300

    using the new collection of metadata

--- src/slangYulParser.ts
+++ src/slangYulParser.ts
@@ -1,11 +1,10 @@
 // https://prettier.io/docs/en/plugins.html#parsers
 import { YulBlock as SlangYulBlock } from '@nomicfoundation/slang/ast';
-import { clearOffsets } from './slang-nodes/SlangNode.js';
 import { createParser } from './slang-utils/create-parser.js';
 import { YulBlock } from './slang-nodes/YulBlock.js';
 
 import type { ParserOptions } from 'prettier';
-import type { AstNode } from './slang-nodes/types.d.ts';
+import type { AstNode, Comment } from './slang-nodes/types.d.ts';
 
 export default function parse(
   text: string,
@@ -15,10 +14,15 @@ export default function parse(
 
   // We update the compiler version by the inferred one.
   options.compiler = parser.languageVersion;
+  const comments: Comment[] = [];
   const parsed = new YulBlock(
     new SlangYulBlock(parseOutput.tree.asNonterminalNode()),
+    { offsets: new Map<number, number>(), comments },
     options
   );
-  clearOffsets();
+
+  // Because of comments being extracted like a Russian doll, the order needs
+  // to be fixed at the end.
+  parsed.comments = comments.sort((a, b) => a.loc.start - b.loc.start);
   return parsed;
 }
