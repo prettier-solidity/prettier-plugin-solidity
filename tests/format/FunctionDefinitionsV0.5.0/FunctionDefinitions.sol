e5a3cc5ed174799fb26e70f95bf13d556bfde063
Klaus <klahott@gmail.com>
2026-02-24 21:34:02 -0300

    clean empty arguments in modifier

--- tests/format/FunctionDefinitionsV0.5.0/FunctionDefinitions.sol
+++ tests/format/FunctionDefinitionsV0.5.0/FunctionDefinitions.sol
@@ -2,4 +2,5 @@ contract FunctionDefinitions {
   function () external {}
   function () external payable {}
   function () external modify(123) payable {}
+  function () external modify() payable {}
 }
