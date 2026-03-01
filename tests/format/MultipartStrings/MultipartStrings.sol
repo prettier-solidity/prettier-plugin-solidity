ca61103f40afc8cff59e47cd4372ad48afdc3caf
Klaus <klahott@gmail.com>
2026-03-01 17:16:53 -0300

    multiline strings should be indented and grouped

--- tests/format/MultipartStrings/MultipartStrings.sol
+++ tests/format/MultipartStrings/MultipartStrings.sol
@@ -1,10 +1,25 @@
 contract MultipartStrings {
-  bytes b1 = hex'beef';
-  bytes b2 = hex"beef";
-  bytes b3 = hex"beef" hex"c0ffee";
-  bytes b4 = hex"beeeeeeeeeeeeeeeeeeeeeef" hex"c0000000000ffeeeeeeeeeeeeeeeeeee";
+  bytes h1 = hex'beef';
+  bytes h2 = hex"beef";
+  bytes h3 = hex"beef" hex"c0ffee";
+  bytes h4 = hex"beeeeeeeeeeeeeeeeeeeeeef" hex"c0000000000ffeeeeeeeeeeeeeeeeeee";
 
   string s1 = "foo";
   string s2 = "foo" "bar";
   string s3 = "foofoofoofooofoofoofofoooofofoo" "barbarbrabrbarbarbabrabrbabr";
+
+  string u1 = unicode'hello 🦄';
+  string u2 = unicode'hello 🦄' unicode' world 🦄';
+  string u3 = unicode'hellohellohellohellohellohello 🦄' unicode' worldworldworldworldworldworld 🦄';
+
+  function multilineString() public pure {
+    bytes hex1 = hex'DeadBeef00' hex'DeadBeef01' hex'DeadBeef02' hex'DeadBeef03' hex'DeadBeef04' hex'DeadBeef05';
+    hex1 = hex'DeadBeef0a' hex'DeadBeef0b' hex'DeadBeef0c' hex'DeadBeef0d' hex'DeadBeef0e' hex'DeadBeef0f';
+
+    string str = 'DeadBeef00' 'DeadBeef01' 'DeadBeef02' 'DeadBeef03' 'DeadBeef04' 'DeadBeef05';
+    str = 'DeadBeef0a' 'DeadBeef0b' 'DeadBeef0c' 'DeadBeef0d' 'DeadBeef0e' 'DeadBeef0f';
+
+    string uni = unicode'DeadBeef00🦄' unicode'DeadBeef01🦄' unicode'DeadBeef02🦄' unicode'DeadBeef03🦄' unicode'DeadBeef04🦄' unicode'DeadBeef05🦄';
+    uni = unicode'DeadBeef0a🦄' unicode'DeadBeef0b🦄' unicode'DeadBeef0c🦄' unicode'DeadBeef0d🦄' unicode'DeadBeef0e🦄' unicode'DeadBeef0f🦄';
+  }
 }
