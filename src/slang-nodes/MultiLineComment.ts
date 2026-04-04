a81e55f3dce9b4f6b31b40948092a81901023ae3
Klaus Hott Vidal <klahott@gmail.com>
2026-04-04 16:38:43 -0300

    Splitting BlockComments into lines only once per run. (#1483)
    * Splitting BlockComments into lines only once per run.
    
    * small rebase to organise readability
    
    * avoid trimStart twice
    
    * small refactor
    
    * iterate only once over lines and exit early if not successful

--- src/slang-nodes/MultiLineComment.ts
+++ src/slang-nodes/MultiLineComment.ts
@@ -1,13 +1,9 @@
 import { TerminalKind } from '@nomicfoundation/slang/cst';
-import { doc } from 'prettier';
-import { isIndentableBlockComment } from '../slang-utils/is-indentable-block-comment.js';
-import { printIndentableBlockComment } from '../slang-printers/print-indentable-block-comment.js';
+import { printBlockComment } from '../slang-printers/print-block-comment.js';
 import { CommentNode } from './CommentNode.js';
 
 import type { TerminalNode } from '@nomicfoundation/slang/cst';
 import type { Doc } from 'prettier';
-
-const { join, literalline } = doc.builders;
 
 export class MultiLineComment extends CommentNode {
   readonly kind = TerminalKind.MultiLineComment;
@@ -21,9 +17,6 @@ export class MultiLineComment extends CommentNode {
   }
 
   print(): Doc {
-    if (isIndentableBlockComment(this)) {
-      return printIndentableBlockComment(this);
-    }
-    return join(literalline, this.value.split('\n'));
+    return printBlockComment(this);
   }
 }
---
+++ src/slang-printers/print-block-comment.ts
@@ -0,0 +1,42 @@
+import { doc } from 'prettier';
+
+import type { Doc } from 'prettier';
+import type { BlockComment } from '../slang-nodes/types.d.ts';
+
+const { hardline, join, literalline } = doc.builders;
+
+function trimmedIndentableLines(lines: string[]): string[] | undefined {
+  // If the comment has multiple lines and every line starts with a star
+  // we can fix the indentation of each line.
+  if (lines.length > 1) {
+    const trimmedLines = [];
+    for (let line of lines) {
+      line = line.trimStart();
+      if (!line.startsWith('*')) {
+        return;
+      }
+      trimmedLines.push(line);
+    }
+    return trimmedLines;
+  }
+}
+
+function printIndentableBlockComment(lines: string[]): Doc {
+  return join(
+    hardline,
+    lines.map((line, index) => `${index === 0 ? '' : ' '}${line.trimEnd()}`)
+  );
+}
+
+export function printBlockComment(comment: BlockComment): Doc {
+  // We remove the initial `/` to check if every line starts with `*`
+  const lines = comment.value.slice(1).split('\n');
+  const trimmedLines = trimmedIndentableLines(lines);
+
+  return [
+    '/',
+    trimmedLines
+      ? printIndentableBlockComment(trimmedLines)
+      : join(literalline, lines)
+  ];
+}
