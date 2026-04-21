1641b77e749c1b9a684be8ae0e701ade7039a1c8
Klaus <klahott@gmail.com>
2026-04-21 00:44:18 -0400

    statement with indented body consolidation

--- src/slang-nodes/ForStatement.ts
+++ src/slang-nodes/ForStatement.ts
@@ -1,13 +1,11 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { doc } from 'prettier';
 import { printSeparatedList } from '../slang-printers/print-separated-list.js';
-import { printIndentedGroupOrSpacedDocument } from '../slang-printers/print-indented-group-or-spaced-document.js';
 import { extractVariant } from '../slang-utils/extract-variant.js';
-import { SlangNode } from './SlangNode.js';
+import { StatementWithIndentedBody } from './StatementWithIndentedBody.js';
 import { ForStatementInitialization } from './ForStatementInitialization.js';
 import { ForStatementCondition } from './ForStatementCondition.js';
 import { Expression } from './Expression.js';
-import { Statement } from './Statement.js';
 
 import type * as ast from '@nomicfoundation/slang/ast';
 import type { Doc } from 'prettier';
@@ -15,7 +13,7 @@ import type { CollectedMetadata, PrintFunction } from '../types.d.ts';
 
 const { line } = doc.builders;
 
-export class ForStatement extends SlangNode {
+export class ForStatement extends StatementWithIndentedBody {
   readonly kind = NonterminalKind.ForStatement;
 
   initialization: ForStatementInitialization['variant'];
@@ -23,8 +21,6 @@ export class ForStatement extends SlangNode {
   condition: ForStatementCondition['variant'];
 
   iterator?: Expression['variant'];
-
-  body: Statement['variant'];
 
   constructor(ast: ast.ForStatement, collected: CollectedMetadata) {
     super(ast, collected);
@@ -38,7 +34,6 @@ export class ForStatement extends SlangNode {
     if (ast.iterator) {
       this.iterator = extractVariant(new Expression(ast.iterator, collected));
     }
-    this.body = extractVariant(new Statement(ast.body, collected));
 
     this.updateMetadata(
       this.initialization,
@@ -62,10 +57,7 @@ export class ForStatement extends SlangNode {
             : ''
       }),
       ')',
-      printIndentedGroupOrSpacedDocument(
-        print('body'),
-        this.body.kind !== NonterminalKind.Block
-      )
+      this.printBody(print)
     ];
   }
 }
