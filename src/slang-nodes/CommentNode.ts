80f21922d172a94dd1720c176a7a5aa29edde0ab
Klaus <klahott@gmail.com>
2026-04-22 14:09:13 -0400

    protecting abstract constructors

--- src/slang-nodes/CommentNode.ts
+++ src/slang-nodes/CommentNode.ts
@@ -2,7 +2,7 @@ import type { TerminalNode } from '@nomicfoundation/slang/cst';
 import type { Location } from '../types.d.ts';
 import type { PrintableNode } from './types.d.ts';
 
-export class CommentNode {
+export abstract class CommentNode {
   loc: Location;
 
   leading?: boolean;
@@ -19,7 +19,7 @@ export class CommentNode {
 
   followingNode?: PrintableNode;
 
-  constructor(ast: TerminalNode, offset: number) {
+  protected constructor(ast: TerminalNode, offset: number) {
     this.loc = {
       start: offset,
       end: offset + ast.textLength.utf16
