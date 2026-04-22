803c77f8c96edc6281b3388ab8fd2c05712bfb5b
Klaus <klahott@gmail.com>
2026-04-22 10:17:39 -0400

    variant collection consolidation

--- src/slang-nodes/LibraryMembers.ts
+++ src/slang-nodes/LibraryMembers.ts
@@ -1,7 +1,6 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { printIndentedPreservingEmptyLines } from '../slang-printers/print-preserving-empty-lines.js';
-import { extractVariant } from '../slang-utils/extract-variant.js';
-import { SlangNode } from './SlangNode.js';
+import { VariantCollection } from './VariantCollection.js';
 import { ContractMember } from './ContractMember.js';
 
 import type * as ast from '@nomicfoundation/slang/ast';
@@ -9,17 +8,14 @@ import type { AstPath, Doc, ParserOptions } from 'prettier';
 import type { CollectedMetadata, PrintFunction } from '../types.d.ts';
 import type { PrintableNode } from './types.d.ts';
 
-export class LibraryMembers extends SlangNode {
+export class LibraryMembers extends VariantCollection<
+  ast.LibraryMembers,
+  ContractMember
+> {
   readonly kind = NonterminalKind.LibraryMembers;
 
-  items: ContractMember['variant'][];
-
   constructor(ast: ast.LibraryMembers, collected: CollectedMetadata) {
-    super(ast, collected, true);
-
-    this.items = ast.items.map((item) =>
-      extractVariant(new ContractMember(item, collected))
-    );
+    super(ast, collected, ContractMember);
   }
 
   print(
