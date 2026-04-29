2e3672fcbf288d4728ffa8a671bd8d469ff06f43
Klaus <klahott@gmail.com>
2026-04-29 17:09:32 -0400

    polymorphic string consolidation

--- src/slang-nodes/HexStringLiteral.ts
+++ src/slang-nodes/HexStringLiteral.ts
@@ -1,25 +1,18 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
 import { printString } from '../slang-printers/print-string.js';
-import { SlangNode } from './SlangNode.js';
+import { PolymorphicString } from './PolymorphicString.js';
 
 import type * as ast from '@nomicfoundation/slang/ast';
-import type { Doc } from 'prettier';
 import type { CollectedMetadata } from '../types.d.ts';
 
-export class HexStringLiteral extends SlangNode {
+export class HexStringLiteral extends PolymorphicString {
   readonly kind = NonterminalKind.HexStringLiteral;
 
-  variant: string;
-
   constructor(ast: ast.HexStringLiteral, collected: CollectedMetadata) {
-    super(ast, collected);
-
-    this.variant = ast.variant.unparse();
-
-    this.variant = `hex${printString(this.variant.slice(4, -1), collected.options)}`;
-  }
-
-  print(): Doc {
-    return this.variant;
+    super(
+      ast,
+      collected,
+      (raw) => `hex${printString(raw.slice(4, -1), collected.options)}`
+    );
   }
 }
