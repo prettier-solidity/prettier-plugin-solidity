681df6000db33a8686d7ecceb5277bc1a4080703
Klaus <klahott@gmail.com>
2026-04-29 17:20:55 -0400

    polymorphic terminal node consolidation

--- src/slang-nodes/FunctionTypeAttribute.ts
+++ src/slang-nodes/FunctionTypeAttribute.ts
@@ -1,18 +1,13 @@
 import { NonterminalKind } from '@nomicfoundation/slang/cst';
-import { SlangNode } from './SlangNode.js';
-import { TerminalNode } from './TerminalNode.js';
+import { PolymorphicTerminalNode } from './PolymorphicTerminalNode.js';
 
 import type * as ast from '@nomicfoundation/slang/ast';
 import type { CollectedMetadata } from '../types.d.ts';
 
-export class FunctionTypeAttribute extends SlangNode {
+export class FunctionTypeAttribute extends PolymorphicTerminalNode {
   readonly kind = NonterminalKind.FunctionTypeAttribute;
-
-  variant: TerminalNode;
 
   constructor(ast: ast.FunctionTypeAttribute, collected: CollectedMetadata) {
     super(ast, collected);
-
-    this.variant = new TerminalNode(ast.variant, collected);
   }
 }
