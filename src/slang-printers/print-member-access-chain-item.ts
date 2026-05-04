67ebd59b5cdec08c06552bd6f8ea6cbf2edb8a47
Klaus Hott Vidal <klahott@gmail.com>
2026-05-04 10:35:59 -0400

    Reusing label instead of creating a new one (#1538)

--- src/slang-printers/print-member-access-chain-item.ts
+++ src/slang-printers/print-member-access-chain-item.ts
@@ -1,10 +1,7 @@
-import { doc } from 'prettier';
 import { isLabel } from '../slang-utils/is-label.js';
 import { printGroupAndIndentIfBreakPair } from './print-group-and-indent-if-break-pair.js';
 
 import type { Doc } from 'prettier';
-
-const { label } = doc.builders;
 
 export const memberAccessChainLabel = Symbol('MemberAccessChain');
 
@@ -15,12 +12,12 @@ export function printPossibleMemberAccessChainItem(
   // If we are at the end of a MemberAccessChain we should indent the
   // arguments accordingly.
   if (isLabel(operand, memberAccessChainLabel)) {
-    // We wrap the expression in a label in case there is an IndexAccess or
-    // a FunctionCall following Node.
-    return label(
-      memberAccessChainLabel,
-      printGroupAndIndentIfBreakPair(operand.contents, predicate)
-    );
+    // We keep the label and update the contents in case there is an
+    // IndexAccess or a FunctionCall following this Node.
+    return {
+      ...operand,
+      contents: printGroupAndIndentIfBreakPair(operand.contents, predicate)
+    };
   }
 
   return [operand, predicate].flat();
