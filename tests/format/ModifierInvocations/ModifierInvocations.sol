5210dd3fc5bdcb0d91d8e858a55abe11a49cfe5e
Klaus <klahott@gmail.com>
2026-07-10 11:10:05 +1200

    passing tests

--- tests/format/ModifierInvocations/ModifierInvocations.sol
+++ tests/format/ModifierInvocations/ModifierInvocations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.35;
+pragma solidity 0.8.36;
 
 contract ModifierDefinitions {
   // We enforce the use of parentheses in modifiers without parameters.
--- tests/format/Parentheses/ShiftRNoParentheses.sol
+++ tests/format/Parentheses/ShiftRNoParentheses.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.35;
+pragma solidity 0.8.36;
 
 contract ShiftRNoParentheses {
     function shiftRAdd(uint256 a, uint256 b, uint256 c)
