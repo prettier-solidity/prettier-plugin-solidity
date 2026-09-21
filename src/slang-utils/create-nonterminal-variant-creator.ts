2d1e96b1672ed4e37d0e2a14f36bf2f11e258171
Klaus <klahott@gmail.com>
2026-09-21 12:21:26 -0300

    tighter types removing the use of any

--- src/slang-utils/create-nonterminal-variant-creator.ts
+++ src/slang-utils/create-nonterminal-variant-creator.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { extractVariant } from './extract-variant.js';
 
 import type { StrictPolymorphicNode } from '../slang-nodes/types.d.ts';
@@ -8,8 +7,13 @@ import type {
   SlangAstNodeClass
 } from '../types.d.ts';
 
-type NodeConstructor<T> = new (ast: any, collected: CollectedMetadata) => T;
 type SlangPolymorphicNode = Extract<SlangAstNode, { variant: unknown }>;
+type ConstructorEntry<
+  Class extends SlangAstNodeClass,
+  T
+> = Class extends unknown
+  ? [Class, new (ast: InstanceType<Class>, collected: CollectedMetadata) => T]
+  : never;
 
 type NonterminalVariantFactory<
   U extends SlangPolymorphicNode,
@@ -20,12 +24,18 @@ export function createNonterminalVariantSimpleCreator<
   U extends SlangPolymorphicNode,
   T extends StrictPolymorphicNode
 >(
-  constructors: [SlangAstNodeClass, NodeConstructor<T['variant']>][]
+  constructors: ConstructorEntry<SlangAstNodeClass, T['variant']>[]
 ): NonterminalVariantFactory<U, T> {
   return (variant, collected) => {
     for (const [slangAstClass, constructor] of constructors) {
-      if (variant instanceof slangAstClass)
-        return new constructor(variant, collected);
+      if (variant instanceof slangAstClass) {
+        return new (
+          constructor as new (
+            ast: typeof variant,
+            collected: CollectedMetadata
+          ) => T['variant']
+        )(variant, collected);
+      }
     }
 
     throw new Error(`Unexpected variant: ${JSON.stringify(variant)}`);
@@ -36,11 +46,11 @@ export function createNonterminalVariantCreator<
   U extends SlangPolymorphicNode,
   T extends StrictPolymorphicNode
 >(
-  constructors: [SlangAstNodeClass, NodeConstructor<T['variant']>][],
-  extractVariantConstructors: [
+  constructors: ConstructorEntry<SlangAstNodeClass, T['variant']>[],
+  extractVariantConstructors: ConstructorEntry<
     SlangAstNodeClass,
-    NodeConstructor<StrictPolymorphicNode>
-  ][]
+    StrictPolymorphicNode
+  >[]
 ): NonterminalVariantFactory<U, T> {
   const simpleCreator = createNonterminalVariantSimpleCreator<U, T>(
     constructors
@@ -48,8 +58,16 @@ export function createNonterminalVariantCreator<
 
   return (variant, collected) => {
     for (const [slangAstClass, constructor] of extractVariantConstructors) {
-      if (variant instanceof slangAstClass)
-        return extractVariant(new constructor(variant, collected));
+      if (variant instanceof slangAstClass) {
+        return extractVariant(
+          new (
+            constructor as new (
+              ast: typeof variant,
+              collected: CollectedMetadata
+            ) => StrictPolymorphicNode
+          )(variant, collected)
+        );
+      }
     }
 
     return simpleCreator(variant, collected);
